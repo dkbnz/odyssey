3702de12e7c912307cf9524c0d8ff915b41b0e85
Doug <dba62@uclive.ac.nz>
2019-03-13 18:58:46 +1300

    Implemented an authorisation controller to handle sessions. Contains simple login/logout methods
    #story[209]

---
+++ app/controllers/AuthController.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import models.Profile;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.Result;
+
+/**
+ * Controller to handle the authorisation of clients.
+ * Sets session cookies and
+ */
+public class AuthController extends Controller {
+
+    /**
+     * Checks if client is authorized, if authorized, return ok()
+     * If not, checks if user exists in the database.
+     * If exists, set session and return okay
+     * Any other cases return appropriate error
+     *
+     * @param request HTTP request from the client
+     * @return Result of users request
+     */
+    public Result login(Http.Request request) {
+        return request.session()
+                .getOptional("authorized")
+                .map(userId -> ok("OK")) // User is already logged in, return OK
+                .orElseGet(() -> {
+                    // User is not logged in, attempt to search database
+                    JsonNode json = request.body().asJson();
+
+                    if (!(json.has("user") && json.has("pass"))) {
+                        // If JSON Object contains no user or pass key, return bad request
+                        // Prevents null pointer exceptions when trying to get the values below.
+                        return badRequest();
+                    }
+
+                    String username = json.get("user").asText();
+                    String password = json.get("pass").asText();
+
+                    Profile profile = Profile.find.query().where()
+                            .like("username", username).findOne();
+
+                    if ((profile != null) && (profile.password.equals(password))) {
+                        // Profile was successfully fetched and password matches,
+                        // Set session token as id and return ok (200 response)
+                        return ok().addingToSession(request, "connected", profile.id.toString());
+                    }
+
+                    return unauthorized("User/pass not found");
+
+                });
+    }
+
+    /**
+     * Clears session resulting in authorized tag being cleared,
+     * Therefore deauthorizing client
+     * @return ok() result, as logout should always succeed
+     */
+    public Result logout() {
+        return ok("OK").withNewSession(); // Sets a new session, clearing the old one
+    }
+
+}
