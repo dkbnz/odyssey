a4310f9b52d0e6a752cfeea01090609c387b764c
Matilda Porterfield <mpo74@uclive.ac.nz>
2019-03-19 15:07:24 +1300

    WIP. Can search destinations on latitude, longitude, country and name. Still need to work on searching by type.

--- app/controllers/destinations/DestinationController.java
+++ app/controllers/destinations/DestinationController.java
@@ -9,14 +9,17 @@ import play.mvc.Http;
 import play.mvc.Result;
 
 import java.util.List;
+import java.util.Map;
 
 public class DestinationController extends Controller {
 
-    private static String ID = "id";
-    private static String NAME = "name";
-    private static String TYPE = "type";
-    private static String COUNTRY = "country";
-    private static String DISTRICT = "district";
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String TYPE = "type";
+    private static final String COUNTRY = "country";
+    private static final String DISTRICT = "district";
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE = "longitude";
 
     /**
      * Fetches all destinations.
@@ -34,31 +37,39 @@ public class DestinationController extends Controller {
      */
     public Result fetch(Http.Request request) {
 
-        //If there is no request body, return all destinations.
-        if (request.body() != null) {
+        //If there are no query parameters, return all destinations.
+        if (request.queryString().isEmpty()) {
             return fetch();
         }
 
         //Filter destinations based on query parameters.
-        JsonNode json = request.body().asJson();
-        // name, type, latitude, longitude, country
-
+        Map<String, String[]> queryString = request.queryString();
         List<Destination> destinations;
 
         ExpressionList<Destination> expressionList = Destination.find.query().where();
-        if (json.has(NAME)) {
-            expressionList.ilike(NAME, json.get(NAME).asText());
+        if (queryString.containsKey(NAME)) {
+            expressionList.ilike(NAME, queryComparator(queryString.get(NAME)[0]));
         }
-        if (json.has(TYPE)) {
-            expressionList.ieq(TYPE, json.get(TYPE).asText());
+        if (queryString.containsKey(TYPE)) {
+            expressionList.ilike(TYPE, queryString.get(TYPE)[0]);
         }
-        if (json.has("latitude")) {
-            expressionList.eq("latitude", json.get("latitude").asLong());
+        if (queryString.containsKey(LATITUDE)) {
+            expressionList.eq(LATITUDE, Double.parseDouble(queryString.get(LATITUDE)[0]));
+        }
+        if (queryString.containsKey(LONGITUDE)) {
+            expressionList.eq(LONGITUDE, Double.parseDouble(queryString.get(LONGITUDE)[0]));
+        }
+        if (queryString.containsKey(COUNTRY)) {
+            expressionList.ilike(COUNTRY, queryComparator(queryString.get(COUNTRY)[0]));
         }
 
         destinations = expressionList.findList();
 
         return ok(Json.toJson(destinations));
+    }
+
+    private String queryComparator(String field) {
+        return "%" + field + "%";
     }
 
     /**
@@ -88,8 +99,8 @@ public class DestinationController extends Controller {
         destination.setName(json.get(NAME).asText());
         destination.setCountry(json.get(COUNTRY).asText());
         destination.setDistrict(json.get(DISTRICT).asText());
-        destination.setLatitude(json.get("crd_latitude").asDouble());
-        destination.setLongitude(json.get("crd_longitude").asDouble());
+        destination.setLatitude(json.get(LATITUDE).asDouble());
+        destination.setLongitude(json.get(LONGITUDE).asDouble());
         destination.setType(DestinationType.valueOf(json.get(TYPE).asText().toUpperCase()));
         return destination;
     }
@@ -130,8 +141,8 @@ public class DestinationController extends Controller {
         oldDestination.setName(json.get(NAME).asText());
         oldDestination.setCountry(json.get(COUNTRY).asText());
         oldDestination.setDistrict(json.get(DISTRICT).asText());
-        oldDestination.setLatitude(json.get("crd_latitude").asDouble());
-        oldDestination.setLongitude(json.get("crd_longitude").asDouble());
+        oldDestination.setLatitude(json.get(LATITUDE).asDouble());
+        oldDestination.setLongitude(json.get(LONGITUDE).asDouble());
         oldDestination.setType(DestinationType.valueOf(json.get(TYPE).asText().toUpperCase()));
 
         oldDestination.update();
