219e9a43bcffe330585e51a96c9d791fc983e5f2
Vinnie <vjj14@uclive.ac.nz>
2019-03-20 15:45:57 +1300

    Fixed a validation issue experienced earlier where the error message was failing to disappear as desired. This was caused by our code in the ajax error function not differentiating between the two response codes it was receiving. This has been fixed, and the error message for password validation has been redone to adhere to this new format #story[209]

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -23,6 +23,9 @@ $(document).ready(function () {
     });
 
 
+    /**
+     *  Upon deselecting input container, check the text entry from the input container against the database to see if their proposed username is already taken
+     */
     $('#username').focusout(function() {
         $("#err_username").remove();
         $.ajax({
@@ -31,18 +34,49 @@ $(document).ready(function () {
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
             data: JSON.stringify({username : $("#username").val()}),
-            success: function (message) {
-                alert("SUCCESS ALERT");
-                //$("#err_username").remove();
-            },
-            error: function () { // "Called if the request fails"
-
-                $('#user_group').append("\n" +
-                    "                    <div id=\"err_username\" class=\"alert alert-danger hide\">\n" +
-                    "                        <strong>Username taken!</strong> Please use another username\n" +
-                    "                    </div>");
-            },
+            error: function (jqXHR) { // "Called if the request fails"
+                if (jqXHR.status === 200) {
+                    $("#err_username").remove();
+                } else if (jqXHR.status === 400) {
+                    $('#username_group').append("\n" +
+                        "                    <div id=\"err_username\" class=\"alert alert-danger \">\n" +
+                        "                        <strong>Username taken!</strong> Please use another username\n" +
+                        "                    </div>");
+                }
+            }
         });
+    });
+
+
+    /**
+     * Upon deselecting password input container, check matching password retype to determine error message visibility.
+     */
+    $('#password').focusout(function() {
+        $("#err_password").remove();
+        if (!($("#password").val() === $("#password_retyped").val())) {
+            $("#password_group").append("\n" +
+                "                        <div id=\"err_password\" class=\"alert alert-danger\" >\n" +
+                "                            <strong>Passwords do not match!</strong> Please ensure this matches your password!\n" +
+                "                        </div>");
+        } else {
+            $("#err_password").remove();
+        }
+    });
+
+
+    /**
+     * Upon deselecting password_retyped input container, check matching password retype to determine error message visibility.
+     */
+    $('#password_retyped').focusout(function() {
+        $("#err_password").remove();
+        if (!($("#password").val() === $("#password_retyped").val())) {
+            $("#password_group").append("\n" +
+                "                        <div id=\"err_password\" class=\"alert alert-danger\" >\n" +
+                "                            <strong>Passwords do not match!</strong> Please ensure this matches your password!\n" +
+                "                        </div>");
+        } else {
+            $("#err_password").remove();
+        }
     });
 
 
