c4909bad17d820976637d1bb1f3950b47c17369a
Doug Barrett <mail+git@dkb.nz>
2019-03-21 18:38:44 +1300

    Code reordering and documentation added /api/ prefix to files #story[209]

--- app/controllers/TravellerTypeController.java
+++ app/controllers/TravellerTypeController.java
@@ -9,8 +9,17 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import java.util.List;
 
+/**
+ * Controller to handle CRUD of nationalities,
+ * As nationalities is primarily a reference table (static),
+ * These actions should only be performed by an admin.
+ */
 public class TravellerTypeController extends Controller {
 
+    /**
+     * Pulls a list of TravellerTypes from the database and returns it as a JSON list
+     * @return Http Result with a json body
+     */
     public Result list() {
 
         ObjectMapper mapper = new ObjectMapper();
