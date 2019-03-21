586ae5611eeb4d0202953f0fc32b6f482e82f2d9
Doug Barrett <mail+git@dkb.nz>
2019-03-21 18:08:51 +1300

    Dropdowns populating from database. Added NationalityController and PassportController for future admin exapansion
    #story[209]

--- app/controllers/NationalityController.java
+++ app/controllers/NationalityController.java
@@ -3,30 +3,34 @@ package controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.Nationality;
 import models.TravellerType;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+
 import java.util.List;
 
-public class TravellerTypeController extends Controller {
+/**
+ * Controller to handle CRUD of nationalities,
+ * As nationalities is primarily a reference table (static),
+ * These actions should only be performed by an admin
+ */
+public class NationalityController extends Controller {
 
     public Result list() {
 
         ObjectMapper mapper = new ObjectMapper();
         ArrayNode results = mapper.createArrayNode();
-        ObjectNode travTypeobj;
+        ObjectNode nationalityobj;
 
-        List<TravellerType> travTypes = TravellerType.find.all();
+        List<Nationality> nationalities = Nationality.find.all();
 
-
-        for(TravellerType travtype : travTypes){
-            travTypeobj = (ObjectNode) Json.toJson(travtype);
-            travTypeobj.remove("profiles");
-            results.add(travTypeobj);
+        for(Nationality nationality : nationalities){
+            results.add(Json.toJson(nationality));
         }
 
         return ok(results);
     }
-
 }
+
