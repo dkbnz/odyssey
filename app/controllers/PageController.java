8b7f913b739833f5b34bd080973c9c2cfd3d33fc
Doug Barrett <mail+git@dkb.nz>
2019-03-22 17:03:06 +1300

    Made dashboard page to be logged into
    #story[209]

--- app/controllers/PageController.java
+++ app/controllers/PageController.java
@@ -4,13 +4,19 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 /**
- * Display Welcome/Login page
+ * Controller to handle the serving of pages
  */
 public class PageController extends Controller {
+
     /**
-     * Handle default path requests
+     * Method to serve the index page
      */
     public Result index() {
         return ok(views.html.index.render());
     }
+
+    /**
+     * Method to serve the dashboard page
+     */
+    public Result dash() { return ok(views.html.dashboard.render()); }
 }
