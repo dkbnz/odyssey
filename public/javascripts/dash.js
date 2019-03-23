26f07c285687d93c961bcb94b5da0fc8e0932997
Isaac Worsley <itw21@uclive.ac.nz>
2019-03-24 11:24:53 +1300

    Fixed carousel links not working correctly for Traveler Types. Some minor styling changes to the modals. Added table to People page. #implement #story[209]

--- public/javascripts/dash.js
+++ public/javascripts/dash.js
@@ -1,1 +1,3 @@
 // All javascript functionality for the dashboard goes in here.
+
+//const getAllProfiles = `SELECT Profile.id, Profile.username, Profile.first_name, Profile.middle_name, Profile.last_name, Profile.gender, Profile.date_of_birth, (SELECT nationality FROM nationality LEFT JOIN profile_nationality ON nationality.id = profile_nationality.nationality_id) FROM profile`
