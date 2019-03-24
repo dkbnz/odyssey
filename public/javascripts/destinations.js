c30c1d571d76974922094f76f3c52e5e1b11e3c7
Isaac Worsley <itw21@uclive.ac.nz>
2019-03-25 12:16:04 +1300

    Have got the destinations search populating a table on the frontend, did some minor restyling. Need to fix type search not working and some more restyling. But destinations are found from the database! #implement #story[211] #pair[itw21, mpo74]

---
+++ public/javascripts/destinations.js
@@ -0,0 +1,26 @@
+function searchDestinations() {
+    var name = document.getElementById("dest_name").value;
+    var type = document.getElementById("dest_type").value;
+    var district = document.getElementById("dest_district").value;
+    var latitude = document.getElementById("dest_latitude").value;
+    var longitude = document.getElementById("dest_longitude").value;
+    var country = document.getElementById("dest_country").value;
+
+
+    $.ajax({
+        type: 'GET',
+        url: "api/destinations",
+        data: {'name': name, 'type': type, 'district': district, 'latitude': latitude, 'longitude': longitude, 'country': country},
+        dataType: 'html',
+        success: function(response) {
+            $("#keywords").remove();
+            $("#tableContent").append(response);
+            document.getElementById("keywords").classList.remove("d-none");
+        },
+        error: function(error) {
+            console.log("ERROR");
+            document.getElementById("keywords").classList.remove("d-none");
+            console.log(error);
+        }
+    });
+}
