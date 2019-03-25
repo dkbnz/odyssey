d33aada7b4ebc917dd31bd5a5b332c82907375e7
Isaac Worsley <itw21@uclive.ac.nz>
2019-03-25 16:12:52 +1300

    Have added create destination page, reformatting of general style and navbars. #implement #story[211]

--- public/javascripts/destinations.js
+++ public/javascripts/destinations.js
@@ -1,4 +1,5 @@
 function searchDestinations() {
+    //document.getElementById("tableDestinations").className("tab-pane fade active show");
     var name = document.getElementById("dest_name").value;
     var type = document.getElementById("dest_type").value;
     var district = document.getElementById("dest_district").value;
@@ -17,11 +18,47 @@ function searchDestinations() {
             $("#tableContent").append(response);
             console.log(response);
             document.getElementById("keywords").classList.remove("d-none");
+            document.getElementById("tableDestinations").classList.add("active");
         },
         error: function(error) {
             console.log("ERROR");
             document.getElementById("keywords").classList.remove("d-none");
             console.log(error);
+        }
+    });
+}
+
+
+function hideTable() {
+    document.getElementById("keywords").classList.add("d-none");
+}
+
+
+function createDestination() {
+    var name = document.getElementById("newDest_name").value;
+    var type = document.getElementById("newDest_type").value;
+    var district = document.getElementById("newDest_district").value;
+    var latitude = document.getElementById("newDest_latitude").value;
+    var longitude = document.getElementById("newDest_longitude").value;
+    var country = document.getElementById("newDest_country").value;
+    console.log(name, type, district, latitude, longitude, country);
+
+    $.ajax({
+        method: "POST",
+        url: "/api/destinations",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify({
+            name : name,
+            type : type,
+            district : district,
+            latitude : latitude,
+            longitude : longitude,
+            country : country
+        }),
+        success: function (response) { // "Called if the request succeeds"
+        },
+        error: function (error) { // "Called if the request fails"
+            console.log(error)
         }
     });
 }
