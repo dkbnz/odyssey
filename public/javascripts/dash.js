0c4901ef627401363d94bc522962353d7b198d5b
Doug <dba62@uclive.ac.nz>
2019-03-27 12:29:41 +1300

    Implemented getters and setters. Should have been done as programmed. I was under the impression that play and specifically ebeans had to have attributes public. #story[212]

--- public/javascripts/dash.js
+++ public/javascripts/dash.js
@@ -1,9 +1,24 @@
 // All javascript functionality for the dashboard goes in here.
 
-//const getAllProfiles = `SELECT Profile.id, Profile.username, Profile.first_name, Profile.middle_name, Profile.last_name, Profile.gender, Profile.date_of_birth, (SELECT nationality FROM nationality LEFT JOIN profile_nationality ON nationality.id = profile_nationality.nationality_id) FROM profile`
 
 $(document).ready(function () {
 
+
+    // When logout button clicked, send POST request to logout
+    $("#logout-btn").click(function(){
+        $.ajax({
+            method: "POST",
+            url: "/api/logout",
+            success: function (response) { // "Called if the request succeeds"
+                window.location = "/" // Redirect to index
+            },
+            error: function (error) { // "Called if the request fails"
+                console.log(error)
+            }
+        });
+    });
+
+    // Make ajax call to populate view profile page
     $.ajax({
         type: 'GET',
         url: "api/profile",
@@ -16,6 +31,7 @@ $(document).ready(function () {
         }
     });
 
+    // Make ajax call to populate edit profile page, Add validation event handlers
     $.ajax({
         type: 'GET',
         url: "api/profile/edit",
@@ -36,8 +52,6 @@ $(document).ready(function () {
                 maxHeight: 400
 
             });
-
-
 
 
             /**
@@ -74,21 +88,6 @@ $(document).ready(function () {
                 $("#err_password1").remove();
                 var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{5,15})");
 
-    // When logout button clicked, send POST request to logout
-    $("#logout-btn").click(function(){
-        $.ajax({
-            method: "POST",
-            url: "/api/logout",
-            success: function (response) { // "Called if the request succeeds"
-                window.location = "/" // Redirect to index
-            },
-            error: function (error) { // "Called if the request fails"
-                console.log(error)
-            }
-        });
-    });
-
-});
                 if(!(mediumRegex.test($("#password").val()))) {
                     // If the contents checked are not accepted by the regex, then present an error message below the container.
                     $(".p1").append("\n" +
