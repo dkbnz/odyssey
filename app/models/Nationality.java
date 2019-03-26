c0795e578932ee46b5a724b9fd2fd01616648e78
Doug Barrett <mail+git@dkb.nz>
2019-03-27 12:29:41 +1300

    Implemented getters and setters. Should have been done as programmed. I was under the impression that play and specifically ebeans had to have attributes public. #story[212]

--- app/models/Nationality.java
+++ app/models/Nationality.java
@@ -20,5 +20,13 @@ public class Nationality extends BaseModel {
     public String nationality;
     public String country;
 
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
     public static Finder<Integer, Nationality> find = new Finder<>(Nationality.class);
 }
