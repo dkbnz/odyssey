5bdad52e60bfa0137928708ead17780a456a5f9d
Matilda Porterfield <mpo74@uclive.ac.nz>
2019-03-27 12:18:53 +1300

    Added test step for creating destination. Throws pending exception as not implemented yet. Still having errors with database configuation for testing. #story[211]

--- test/steps/DestinationTestSteps.java
+++ test/steps/DestinationTestSteps.java
@@ -68,4 +68,16 @@ public class DestinationTestSteps {
     public void the_status_code_received_is_OK() {
         assertEquals(OK, statusCode);
     }
+
+    @When("I create a new destination with the following values")
+    public void iCreateANewDestinationWithTheFollowingValues(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new cucumber.api.PendingException();
+    }
 }
