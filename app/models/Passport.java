147304e6d65fab7080b16ee3a811faea70005af4
mjk141 <mjk141@uclive.ac.nz>
2019-03-27 20:46:10 +1300

    Optimised imports for all of the involved classes
    #story[212] #refactor

--- app/models/Passport.java
+++ app/models/Passport.java
@@ -3,8 +3,6 @@ package models;
 import io.ebean.Finder;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Passport extends BaseModel {
