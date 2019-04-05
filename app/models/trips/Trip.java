f0dbb40e5ba0538d440570df0b7529c5b9226536
Joel Ridden <jnr26@uclive.ac.nz>
2019-04-05 15:52:07 +1300

    Implemented the model for trips and trip destinations, linking for foreign keys in database tables
    #story[301]

---
+++ app/models/trips/Trip.java
@@ -0,0 +1,51 @@
+package models.trips;
+
+import io.ebean.Finder;
+import models.BaseModel;
+import play.data.validation.Constraints;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+/**
+ * Class for holding trips for a user with all their trip destinations and trip name
+ */
+@Entity
+public class Trip extends BaseModel {
+
+    /**
+     * The name of the trip.
+     */
+    @Constraints.Required
+    private String name;
+
+    /**
+     * The trips destinations for the trip
+     */
+//    @Constraints.Required
+    @OneToMany(mappedBy = "trip", cascade=CascadeType.ALL)
+    private List<TripDestination> destinations;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<TripDestination> getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(List<TripDestination> destinations) {
+        this.destinations = destinations;
+    }
+
+    /**
+     * A finder used to search for a trip
+     */
+    public static Finder<Integer, Trip> find = new Finder<>(Trip.class);
+}
