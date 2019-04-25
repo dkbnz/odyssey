49f6cd083c50abe63373bfb23860c948c7142e0d
Isaac Worsley <itw21@uclive.ac.nz>
2019-04-25 13:33:20 +1200

    Got bootstrap vue into the application. Started getting components in (Trips.vue). Able to populate a table with hardcoded data, setting it up for the backend functionality. #story[362] #story[302]

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,86 +1,104 @@
 <template>
-  <div class="App">
-    <h1>{{ appSummary }}</h1>
-    <vue-play></vue-play>
-  </div>
+
+    <div class="App">
+        <div>
+            <b-navbar>
+                <b-navbar-brand href="#"><img :src="assets.appLogo"></b-navbar-brand>
+            </b-navbar>
+        </div>
+
+        <trips></trips>
+
+    </div>
 </template>
 <script>
-  import VuePlay from './components/VuePlay.vue'
-  export default {
-    mounted () {
-      this.getSummary(summary => this.appSummary = summary.content)
-    },
-    data () {
-      return {
-        appSummary: ''
-      }
-    },
-    methods: {
-      getSummary (cb) {
-        return fetch(`/v1/summary`, {
-          accept: "application/json"
-        })
-          .then(this.checkStatus)
-          .then(this.parseJSON)
-          .then(cb)
-      },
 
-      checkStatus (response) {
-        if (response.status >= 200 && response.status < 300) {
-          return response;
+    import Trips from './components/Trips.vue'
+    import assets from './assets'
+    export default {
+        computed: {
+            assets () {
+                return assets
+            },
+        },
+        mounted () {
+            this.getSummary(summary => this.appSummary = summary.content)
+        },
+        data () {
+            return {
+                appSummary: ''
+            }
+        },
+        methods: {
+            getSummary (cb) {
+                return fetch(`/v1/summary`, {
+                    accept: "application/json"
+                })
+                    .then(this.checkStatus)
+                    .then(this.parseJSON)
+                    .then(cb)
+            },
+
+            checkStatus (response) {
+                if (response.status >= 200 && response.status < 300) {
+                    return response;
+                }
+                const error = new Error(`HTTP Error ${response.statusText}`);
+                error.status = response.statusText;
+                error.response = response;
+                console.log(error); // eslint-disable-line no-console
+                throw error;
+            },
+
+            parseJSON (response) {
+                return response.json();
+            }
+        },
+        components: {
+            Trips
         }
-        const error = new Error(`HTTP Error ${response.statusText}`);
-        error.status = response.statusText;
-        error.response = response;
-        console.log(error); // eslint-disable-line no-console
-        throw error;
-      },
-
-      parseJSON (response) {
-        return response.json();
-      }
-    },
-    components: {
-      VuePlay
     }
-  }
 </script>
 <style>
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    background-color: rgb(39, 40, 34);
-    color: #f0f8ff;
-  }
-
-  a {
-    color: #00ffff;
-  }
-
-  .App {
-    text-align: center;
-  }
-
-  .App-logo {
-    animation: bounce 0.8s infinite alternate;
-    -webkit-animation: bounce 0.8s infinite alternate;
-  }
-
-  @keyframes bounce {
-    from {
-      transform: translateY(35px);
+    html {
+        /* The html and body elements cannot have any padding or margin. */
+        height: 100%;
     }
-    to {
-      transform: translateY(-25px);
+
+    body {
+        background-color: #e2e6ea;
+        padding-bottom: 50px;
+
     }
-  }
-  @-webkit-keyframes bounce {
-    from {
-      transform: translateY(35px);
+
+    /* Imports the Bauman's Regular Font */
+    @font-face
+    {
+        font-family: Baumans;
+        src: url('../static/Baumans-Regular.ttf');
     }
-    to {
-      transform: translateY(-25px);
+
+    /* Sets the font styling for headings*/
+    h1, h2, h3, h4 {
+        font-family: Baumans, sans-serif;
+        font-weight: bold;
     }
-  }
+
+
+    #footer {
+        background-color: white;
+        text-align: center;
+        position: fixed;
+        left: 0px;
+        bottom: 0px;
+        height: 40px;
+        width: 100%;
+        font-size: 0.7em;
+    }
+
+
+    /* Page Titles and Subtitles*/
+    .page_title {
+        text-align: center;
+    }
 </style>
