68c62ce49e55bdc126a36ed66be0357bea02c262
Isaac Worsley <itw21@uclive.ac.nz>
2019-04-28 21:07:05 +1200

    Fixed adding destinations, so now destination names are checked using the method in the DestinationController. Appropriate error messages are given, but can't get success message to display due to Promises and synchronous issues #story[362]

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,8 +2,8 @@
 
     <div class="App">
         <div>
-            <dash v-if="profile.length !== 0" v-bind:profile="profile" v-bind:destinations="destinations" v-bind:destinationTypes="destinationTypes" v-bind:nationalityOptions="nationalityOptions" v-bind:travTypeOptions="travTypeOptions"></dash>
-            <index v-if="profile.length === 0" v-bind:profile="profile" v-bind:destinations="destinations" v-bind:nationalityOptions="nationalityOptions" v-bind:travTypeOptions="travTypeOptions"></index>
+            <destinations v-if="profile.length !== 0" v-bind:profile="profile" v-bind:destinations="destinations" v-bind:destinationTypes="destinationTypes" v-bind:nationalityOptions="nationalityOptions" v-bind:travTypeOptions="travTypeOptions"></destinations>
+            <index v-if="profile.length === 0" v-bind:profile="profile" v-bind:destinations="destinations" v-bind:destinationTypes="destinationTypes" v-bind:nationalityOptions="nationalityOptions" v-bind:travTypeOptions="travTypeOptions" ></index>
         </div>
     </div>
 </template>
