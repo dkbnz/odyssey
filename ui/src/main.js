847c034bd8d0f1edc21a2726f662a5ff45daf32a
Matilda Porterfield <mpo74@uclive.ac.nz>
2019-04-30 11:55:38 +1200

    Added Vue Router to the project. Added routes for all endpoints. #story[362] #pair[mpo74,itw21]

--- ui/src/main.js
+++ ui/src/main.js
@@ -4,16 +4,59 @@ import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 
+import Index from './components/index/indexPage'
+import Dash from './components/dash/dashPage'
+import Destinations from './components/destinations/destinationsPage'
+import Profiles from './components/profiles/profilesPage'
+import Trips from './components/trips/tripsPage'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+
+const routes = [
+    {
+        path:"/",
+        name: "index",
+        component: Index
+    },
+    {
+        path:"/dash",
+        name: "dash",
+        component: Dash
+    },
+    {
+        path:"/destinations",
+        name: "destinations",
+        component: Destinations
+    },
+    {
+        path:"/profiles",
+        name: "profiles",
+        component: Profiles
+    },
+    {
+        path:"/trips",
+        name: "trips",
+        component: Trips
+    }
+];
+
+const router = new VueRouter({
+    routes: routes,
+    mode: 'history'
+});
 
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
+    router: router,
     template: '<App/>',
     components: { App }
 });
