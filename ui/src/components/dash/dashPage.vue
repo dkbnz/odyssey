40370e8f0d80f886ed1f83be3258ebf49f8c1e9f
Matilda Porterfield <mpo74@uclive.ac.nz>
2019-05-02 16:38:21 +1200

    Removed unnecessary comments and fixed spacing for dash and destination components. #story[362]

--- ui/src/components/dash/dashPage.vue
+++ ui/src/components/dash/dashPage.vue
@@ -7,8 +7,10 @@
                 <b-nav-item @click="togglePage(editProfile)">Edit Profile</b-nav-item>
             </b-navbar-nav>
         </b-navbar>
-        <view-profile v-if="viewProfile" v-bind:profile="profile" v-bind:nationalityOptions="nationalityOptions" v-bind:travTypeOptions="travTypeOptions"></view-profile>
-        <edit-profile v-if="editProfile" v-bind:profile="profile" v-bind:nationalityOptions="nationalityOptions" v-bind:travTypeOptions="travTypeOptions"></edit-profile>
+        <view-profile v-if="viewProfile" v-bind:profile="profile" v-bind:nationalityOptions="nationalityOptions"
+                      v-bind:travTypeOptions="travTypeOptions"></view-profile>
+        <edit-profile v-if="editProfile" v-bind:profile="profile" v-bind:nationalityOptions="nationalityOptions"
+                      v-bind:travTypeOptions="travTypeOptions"></edit-profile>
         <footer-main></footer-main>
     </div>
     <div v-else>
@@ -22,24 +24,25 @@
     import NavBarMain from '../helperComponents/navbarMain.vue'
     import FooterMain from '../helperComponents/footerMain.vue'
     import UnauthorisedPrompt from '../helperComponents/unauthorisedPromptPage'
+
     export default {
         name: "dashPage",
         props: ['profile', 'nationalityOptions', 'travTypeOptions'],
         created() {
             document.title = "TravelEA - Dashboard";
         },
-        data: function() {
+        data: function () {
             return {
                 viewProfile: true,
                 editProfile: false,
             }
         },
-        mounted () {
+        mounted() {
 
         },
         methods: {
-            togglePage: function(viewPage) {
-                if(!viewPage) {
+            togglePage: function (viewPage) {
+                if (!viewPage) {
                     this.viewProfile = !this.viewProfile;
                     this.editProfile = !this.editProfile;
                 }
@@ -54,7 +57,3 @@
         }
     }
 </script>
-
-<style scoped>
-
-</style>
