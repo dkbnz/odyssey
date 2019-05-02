0f389c065f2ac855f1ac03876501382957e549b6
Matthew <mjk141@uclive.ac.nz>
2019-05-02 20:16:31 +1200

    Implemented initial functionality for development code deployment. #story[309]

--- scripts/travelea.sh
+++ scripts/travelea.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 start () {
-    echo $(ls)
+    cd ../target/universal/seng302-team-100-everyware-0.0.1-SNAPSHOT/bin
+    case $1 in
+        production)
+            echo Production
+            ;;
+        development)
+            echo Deploying development...
+            ./seng302-team-100-everyware -Dhttp.port=8443
+            ;;
+esac
 }
 
-case $1 in
+case $2 in
     start)
         start
         ;;
