80e7ee1ea010502bc0a17a6f9f15456daee917bb
Matilda Porterfield <mpo74@uclive.ac.nz>
2019-05-02 16:28:15 +1200

    Removed unnecessary comments. Fixed spacing. Moved style to a css file #story[362]

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-
     <div class="App">
         <div>
             <router-view  v-bind:profile="profile" v-bind:destinations="destinations" v-bind:destinationTypes="destinationTypes" v-bind:nationalityOptions="nationalityOptions" v-bind:travTypeOptions="travTypeOptions"></router-view>
@@ -33,65 +32,44 @@
             }
         },
         methods: {
-            getDestinations (cb) {
+            getDestinations (updateDestinations) {
                 return fetch(`/v1/destinations`, {
                     accept: "application/json"
                 })
-                    //.then(this.checkStatus)
                     .then(this.parseJSON)
-                    .then(cb);
+                    .then(updateDestinations);
             },
-            getDestinationTypes (cb) {
+            getDestinationTypes (updateDestinationTypes) {
                 return fetch(`/v1/destinationTypes`, {
                     accept: "application/json"
                 })
-                //.then(this.checkStatus)
                     .then(this.parseJSON)
-                    .then(cb);
+                    .then(updateDestinationTypes);
             },
-
-
-            getProfile (cb) {
+            getProfile (updateProfile) {
                 return fetch(`/v1/profile`, {
                     accept: "application/json"
                 })
-                    //.then(this.checkStatus)
                     .then(this.parseJSON)
-                    .then(cb);
+                    .then(updateProfile);
             },
-
-            getNationalities (cb) {
+            getNationalities (updateNationalities) {
                 return fetch(`/v1/nationalities`, {
                     accept: "application/json"
                 })
-                    //.then(this.checkStatus)
                     .then(this.parseJSON)
-                    .then(cb);
+                    .then(updateNationalities);
             },
-            getTravTypes (cb) {
+            getTravTypes (updateTravellerTypes) {
                 return fetch(`/v1/travtypes`, {
                     accept: "application/json"
                 })
-                    //.then(this.checkStatus)
                     .then(this.parseJSON)
-                    .then(cb);
-            },
-
-            checkStatus (response) {
-                if (response.status >= 200 && response.status < 300) {
-                    return response;
-                }
-                const error = new Error(`HTTP Error ${response.statusText}`);
-                error.status = response.statusText;
-                error.response = response;
-                console.log(error); // eslint-disable-line no-console
-                throw error;
+                    .then(updateTravellerTypes);
             },
             parseJSON (response) {
                 return response.json();
             }
-
-
         },
         components: {
             Index
@@ -99,52 +77,5 @@
     }
 </script>
 <style>
-    html {
-        /* The html and body elements cannot have any padding or margin. */
-        height: 100%;
-    }
-
-    body {
-        background-color: #e2e6ea;
-        padding-bottom: 50px;
-
-    }
-    .container {
-        margin-top: 20px;
-        padding-top: 20px;
-        padding-bottom: 50px;
-        background-color: white;
-        border-radius: 5px;
-    }
-
-    /* Imports the Bauman's Regular Font */
-    @font-face
-    {
-        font-family: Baumans;
-        src: url('../static/Baumans-Regular.ttf');
-    }
-
-    /* Sets the font styling for headings*/
-    h1, h2, h3, h4 {
-        font-family: Baumans, sans-serif;
-        font-weight: bold;
-    }
-
-
-    #footer {
-        background-color: white;
-        text-align: center;
-        position: fixed;
-        left: 0px;
-        bottom: 0px;
-        height: 40px;
-        width: 100%;
-        font-size: 0.7em;
-    }
-
-
-    /* Page Titles and Subtitles*/
-    .page_title {
-        text-align: center;
-    }
+    @import "css/app.css";
 </style>
