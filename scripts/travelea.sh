ae6d237087c7b2b46a1df6cd9740bb5ce50e7c1e
Matthew <mjk141@uclive.ac.nz>
2019-05-03 00:47:57 +1200

    Updated script to handle production deployment. #story[309]

--- scripts/travelea.sh
+++ scripts/travelea.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-
 start () {
-    cd ../target/universal/seng302-team-100-everyware-0.0.1-SNAPSHOT/bin
+    cd /var/lib/gitlab-runner/builds/69a828c9/0/seng302-2019/team-100/target/universal/seng302-team-100-everyware-0.0.1-SNAPSHOT/bin/
     case $1 in
         production)
-            echo Production
+            echo Deploying production...
+            sudo ./seng302-team-100-everyware -Dhttp.port=443
             ;;
         development)
             echo Deploying development...
-            ./seng302-team-100-everyware -Dhttp.port=8443
+            sudo ./seng302-team-100-everyware -Dhttp.port=8443
             ;;
-esac
+    esac
 }
 
 case $2 in
     start)
-        start
+        start $1
         ;;
 esac
