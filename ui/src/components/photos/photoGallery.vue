8ce641ae79d5130d23bf44ed14059029d7a511c8
Cam <cta67@uclive.ac.nz>
2019-05-03 16:12:57 +1200

    Created a Vue Component for selecting multiple images from file browser to be uploaded. embedded into another new component for photo gallery and added photo gallery as a 3rd tab within the sub-navigation of profile. #implement #story[341]

---
+++ ui/src/components/photos/photoGallery.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div>
+        <photoUploader></photoUploader>
+    </div>
+
+</template>
+
+<script>
+    import photoUploader from "../photos/photoUploader"
+    export default {
+        name: "photoGallery",
+        components: {
+            photoUploader
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
+
