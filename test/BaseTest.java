35a69a791adb284ce6ef16d57a5407b8c2196d49
Isaac Worsley <itw21@uclive.ac.nz>
2019-05-08 15:08:25 +1200

    Finished the current destination test features in cucumber. #pair[mpo74, itw21]

--- test/BaseTest.java
+++ test/BaseTest.java
@@ -1,45 +1,176 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
+import org.junit.Before;
 import play.Application;
 import play.db.Database;
 import play.db.evolutions.Evolutions;
+import play.mvc.Http;
+import play.mvc.Result;
 import play.test.Helpers;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.*;
 
 public class BaseTest {
 
+    /**
+     * Variable to hold the status code of the result.
+     */
+    private int statusCode;
+
+
+    /**
+     * The destination endpoint uri.
+     */
+    private static final String DESTINATION_URI = "/v1/destinations";
+
+
+    /**
+     * The login endpoint uri.
+     */
+    private static final String LOGIN_URI = "/v1/login";
+
+
+    /**
+     * The logout endpoint uri.
+     */
+    private static final String LOGOUT_URI = "/v1/logout";
+
+
+    /**
+     * A valid username for login credentials for admin user.
+     */
+    private static final String VALID_USERNAME = "admin@travelea.com";
+
+
+    /**
+     * A valid password for login credentials for admin user.
+     */
+    private static final String VALID_PASSWORD = "admin1";
+
+
+    /**
+     * String to add the equals character (=) to build a query string.
+     */
+    private static final String EQUALS = "=";
+
+
+    /**
+     * String to add the ampersand character (&) to build a query string.
+     */
+    private static final String AND = "&";
+
+
+    /**
+     * String to add the question mark character (?) to build a query string.
+     */
+    private static final String QUESTION_MARK = "?";
+
+    /**
+     * The fake application.
+     */
+
     protected Application application;
+
+
+    /**
+     * Database instance for the fake application.
+     */
     protected Database database;
 
+    /**
+     * Runs before each test scenario.
+     * Sets up a fake application for testing.
+     * Applies configuration settings to use an in memory database for the fake application.
+     * Starts the application.
+     * Calls apply evolutions to set up the database state.
+     */
     @Before
     public void setUp() {
-//        Map<String, String> configuration = new HashMap<>();
-//        configuration.put("db.default.driver", "org.h2.Driver");
-//        configuration.put("db.default.url", "jdbc:h2:mem:testDB;MODE=MYSQL;");
-//        configuration.put("play.evolutions.db.default.enabled", "true");
-//        configuration.put("play.evolutions.autoApply", "false");
-//
-//        //Set up the fake application to use the in memory database config
-//        application = fakeApplication(configuration);
-//        Helpers.start(application);
-//        database = application.injector().instanceOf(Database.class);
-//
-//        Evolutions.applyEvolutions(
-//                database,
-//                Evolutions.fromClassLoader(
-//                        getClass().getClassLoader(),
-//                        "test/"
-//                )
-//        );
+
+        //Set up the fake application to use the in memory database config
+        application = fakeApplication();
+
+        database = application.injector().instanceOf(Database.class);
+        applyEvolutions();
+
+        Helpers.start(application);
     }
 
+    /**
+     * Applies down evolutions to the database from the test/evolutions/default directory.
+     *
+     * This drops tables and data from the database.
+     */
+    private void applyEvolutions() {
+        Evolutions.applyEvolutions(
+                database,
+                Evolutions.fromClassLoader(
+                        getClass().getClassLoader(),
+                        "test/"
+                )
+        );
+    }
+
+    /**
+     * Runs after each test scenario.
+     * Sends a logout request.
+     * Cleans up the database by cleaning up evolutions and shutting it down.
+     * Stops running the fake application.
+     */
     @After
-    public void cleanEvolutions() {
-//        Evolutions.cleanupEvolutions(database);
-//        Helpers.stop(application);
+    public void tearDown() {
+        logoutRequest();
+        cleanEvolutions();
+        database.shutdown();
+        Helpers.stop(application);
+    }
+
+
+
+    /**
+     * Applies up evolutions to the database from the test/evolutions/default directory.
+     *
+     * This populates the database with necessary tables and values.
+     */
+    private void cleanEvolutions() {
+        Evolutions.cleanupEvolutions(database);
+    }
+
+
+    /**
+     * Sends a fake request to the application to login.
+     * @param username      The string of the username to complete the login with.
+     * @param password      The string of the password to complete the login with.
+     */
+    private void loginRequest(String username, String password) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.createObjectNode();
+
+        ((ObjectNode) json).put("username", username);
+        ((ObjectNode) json).put("password", password);
+
+        Http.RequestBuilder request = fakeRequest()
+                .method(POST)
+                .bodyJson(json)
+                .uri(LOGIN_URI);
+        Result loginResult = route(application, request);
+
+        statusCode = loginResult.status();
+    }
+
+
+    /**
+     * Sends a fake request to the application to logout.
+     */
+    private void logoutRequest() {
+        Http.RequestBuilder request = fakeRequest()
+                .method(POST)
+                .uri(LOGOUT_URI);
+        route(application, request);
     }
 }
