33cc3b5e88a8e4d34127f8d6160af82657a6abfe
Vinnie <vjj14@uclive.ac.nz>
2019-05-08 16:02:48 +1200

    Working on trip cucumber tests
    #story[301] #pair[vjj14,cta67]

---
+++ test/steps/TripTestSteps.java
@@ -0,0 +1,239 @@
+package steps;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import play.Application;
+import play.db.Database;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.Helpers;
+
+
+import static org.junit.Assert.assertEquals;
+import static play.mvc.Http.Status.BAD_REQUEST;
+import static play.mvc.Http.Status.CREATED;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.*;
+
+import play.db.evolutions.Evolutions;
+
+import java.util.List;
+import java.util.Map;
+
+import static play.test.Helpers.fakeApplication;
+
+public class TripTestSteps {
+
+
+    /**
+     * Variable to hold the status code of the result.
+     */
+    private int statusCode;
+
+
+    /**
+     * The destination endpoint uri.
+     */
+    private static final String DESTINATION_URI = "/v1/destinations";
+
+
+    /**
+     * The login endpoint uri.
+     */
+    private static final String LOGIN_URI = "/v1/login";
+
+
+    /**
+     * The logout endpoint uri.
+     */
+    private static final String LOGOUT_URI = "/v1/logout";
+
+
+    /**
+     * A valid username for login credentials for admin user.
+     */
+    private static final String VALID_USERNAME = "admin@travelea.com";
+
+
+    /**
+     * A valid password for login credentials for admin user.
+     */
+    private static final String VALID_PASSWORD = "admin1";
+
+
+    /**
+     * String to add the equals character (=) to build a query string.
+     */
+    private static final String EQUALS = "=";
+
+
+    /**
+     * String to add the ampersand character (&) to build a query string.
+     */
+    private static final String AND = "&";
+
+
+    /**
+     * String to add the question mark character (?) to build a query string.
+     */
+    private static final String QUESTION_MARK = "?";
+
+
+    /**
+     * The fake application.
+     */
+
+    protected Application application;
+
+
+    /**
+     * Database instance for the fake application.
+     */
+    protected Database database;
+
+
+    @Before
+    public void setUp() {
+
+        //Set up the fake application to use the in memory database config
+        application = fakeApplication();
+
+        database = application.injector().instanceOf(Database.class);
+        applyEvolutions();
+
+        Helpers.start(application);
+    }
+
+
+    /**
+     * Runs after each test scenario.
+     * Sends a logout request.
+     * Cleans up the database by cleaning up evolutions and shutting it down.
+     * Stops running the fake application.
+     */
+    @After
+    public void tearDown() {
+        logoutRequest();
+        cleanEvolutions();
+        database.shutdown();
+        Helpers.stop(application);
+    }
+
+
+    /**
+     * Applies down evolutions to the database from the test/evolutions/default directory.
+     *
+     * This drops tables and data from the database.
+     */
+    private void applyEvolutions() {
+        Evolutions.applyEvolutions(
+                database,
+                Evolutions.fromClassLoader(
+                        getClass().getClassLoader(),
+                        "test/"
+                )
+        );
+    }
+
+
+    /**
+     * Applies up evolutions to the database from the test/evolutions/default directory.
+     *
+     * This populates the database with necessary tables and values.
+     */
+    private void cleanEvolutions() {
+        Evolutions.cleanupEvolutions(database);
+    }
+
+
+    /**
+     * Sends a fake request to the application to login.
+     * @param username      The string of the username to complete the login with.
+     * @param password      The string of the password to complete the login with.
+     */
+    private void loginRequest(String username, String password) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.createObjectNode();
+
+        ((ObjectNode) json).put("username", username);
+        ((ObjectNode) json).put("password", password);
+
+        Http.RequestBuilder request = fakeRequest()
+                .method(POST)
+                .bodyJson(json)
+                .uri(LOGIN_URI);
+        Result loginResult = route(application, request);
+
+        statusCode = loginResult.status();
+    }
+
+
+    /**
+     * Sends a fake request to the application to logout.
+     */
+    private void logoutRequest() {
+        Http.RequestBuilder request = fakeRequest()
+                .method(POST)
+                .uri(LOGOUT_URI);
+        route(application, request);
+    }
+
+
+    /**
+     * Asserts the fake application is in test mode.
+     */
+    @Given("The state of the application is that it is running")
+    public void the_state_of_the_application_is_that_it_is_running () {
+        Assert.assertTrue(application.isTest());
+    }
+
+
+    /**
+     * Attempts to send a log in request with user credentials from constants VALID_USERNAME
+     * and VALID_PASSWORD.
+     *
+     * Asserts the login was successful with a status code of OK (200).
+     */
+    @Given("I am logged into the application which is running")
+    public void i_am_logged_into_the_application_which_is_running() {
+        loginRequest(VALID_USERNAME, VALID_PASSWORD);
+        assertEquals(OK, statusCode);
+    }
+
+
+    @When("the following json containing a trip is sent")
+    public void the_following_json_containing_a_trip_is_sent(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+
+        List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
+        String tripName = list.get(0).get("Name");
+
+
+
+        throw new cucumber.api.PendingException();
+    }
+
+
+    /**
+     * Checks if the status code received is Created (201).
+     */
+    @Then("the received status code corresponds with a Created response")
+    public void the_received_status_code_corresponds_with_a_Created_response() {
+        assertEquals(CREATED, statusCode);
+    }
+
+}
