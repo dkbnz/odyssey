140caa5c5474f628d5caf8177a17d499f4ee3f34
Doug Barrett <mail+git@dkb.nz>
2019-05-16 14:45:57 +1200

    Refactoring admin panel and pages. Able to emit a selected profile for an admin to work on. #pair[itw21, dba62] #story[383]

--- ui/src/components/admin/adminPanel.vue
+++ ui/src/components/admin/adminPanel.vue
@@ -1,121 +1,55 @@
 <template>
     <div>
         <nav-bar-main v-bind:profile="profile"></nav-bar-main>
-        <div class="adminPanel">
-            <h1 class="page_title">Welcome to the Admin Panel</h1>
-            <p class="page_title">
-                <i>Because you are an admin, you can achieve all functionality in the application!</i>
-            </p>
-            <b-row>
-                <b-col cols="8">
-                    <b-card
-                            header="Search for Profiles"
-                            header-tag="header">
-                        <!-- Display the search profiles component -->
-                        <profiles-page :key="refreshProfiles" @admin-edit="navigateToSingleProfile" :perPage=5
-                                       :adminView="true" :profile="profile" :nationalityOptions="nationalityOptions"
-                                       :travTypeOptions="travTypeOptions">
-                        </profiles-page>
-
-                    </b-card>
-                </b-col>
-                <b-col>
-                    <b-card header="Create a Profile">
-                        <b-button v-b-toggle.signUpPage block variant="success">Create a New Profile</b-button>
-                        <!-- The collapsible that uses the sign up page to create a new profile -->
-                        <b-collapse id="signUpPage" class="mt-2">
-                            <sign-up :nationalityOptions="nationalityOptions" :travTypeOptions="travTypeOptions"
-                                     :createdByAdmin="true" @profile-created="showOptions">
-                            </sign-up>
-                        </b-collapse>
-                    </b-card>
-                    <b-card
-                    header="Manage Destinations"
-                    style="margin-top: 10px">
-                    Some content
-                    </b-card>
-                </b-col>
-            </b-row>
-        </div>
-        <div>
-            <!-- The modal to show after a profile has been successfully created -->
-            <b-modal
-                    id="optionModal"
-                    ref="optionModal"
-                    hide-footer>
-                <template slot="modal-title"><h2>Profile Successfully Created</h2></template>
-                Would you like to view this profile?
-                <b-row align-h="center" style="margin-top: 10px">
-                    <b-col>
-                        <b-button block variant="primary" @click="hideOptionModal">No</b-button>
-                    </b-col>
-                    <b-col>
-                        <b-button block variant="primary" @click="navigateToSingleProfile">Yes</b-button>
-                    </b-col>
-                </b-row>
-            </b-modal>
-            <single-profile v-if="showSingleProfile"></single-profile>
-        </div>
-
+        <!--<single-profile></single-profile>-->
+        <admin-actions :profile="profile"
+                       :nationalityOptions="nationalityOptions"
+                       :travTypeOptions="travTypeOptions"
+                        @admin-edit="navigateToSingleProfile"
+                       v-if="editProfile === null">
+        </admin-actions>
+        <single-profile
+        :editProfile="editProfile"
+        :profile="profile"
+        v-else>
+        </single-profile>
     </div>
 </template>
 
 <script>
     import NavBarMain from '../helperComponents/navbarMain.vue'
-    import ProfilesPage from '../profiles/profilesPage.vue'
-    import SignUp from '../index/signup.vue'
     import SingleProfile from './singleProfile.vue'
+    import AdminActions from './adminActions.vue'
+
     export default {
         name: "adminPanel",
         props: ['profile', 'nationalityOptions', 'travTypeOptions'],
         data() {
             return {
-                refreshProfiles: 0,
-                showSingleProfile: false
+                showSingleProfile: false,
+                editProfile: null,
+                viewSingleProfile: false
             }
         },
         methods: {
             /**
-             * If a profile is successfully created, then re-render the profiles component (to ensure the new profile
-             * is shown). Will then hide the sign up modal and show the what to do next modal.
-             */
-            showOptions() {
-                this.refreshProfiles += 1;
-                this.$refs['optionModal'].show();
-            },
-
-            /**
-             * Hides the option modal.
-             */
-            hideOptionModal() {
-                this.$refs['optionModal'].hide();
-            },
-
-            /**
              * Navigates to the single profile page where an admin can achieve all functionality of the selected
              * user.
              */
-            navigateToSingleProfile(profile) {
-                console.log(profile);
-                this.$router.push('/singleProfile')
+            navigateToSingleProfile(editProfile) {
+                console.log(editProfile);
+                this.editProfile = editProfile;
+                this.viewSingleProfile = true;
+                //this.$router.push('/singleProfile')
             }
         },
         components: {
             NavBarMain,
-            ProfilesPage,
-            SignUp,
-            SingleProfile
+            SingleProfile,
+            AdminActions
         }
     }
 </script>
 
 <style scoped>
-
-    .adminPanel {
-        background: white;
-        width: 90vw;
-        margin: 2vh 5vw 5vw 5vw;
-        border-radius: 5px;
-        padding: 2vw;
-    }
 </style>
