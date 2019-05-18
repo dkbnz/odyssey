e52c0b3764dd81e272e183432c08037c315a0316
Isaac Worsley <itw21@uclive.ac.nz>
2019-05-18 16:36:45 +1200

    Minor fixes to the dates again. Hopefully this is the last time.

--- ui/src/components/trips/planATrip.vue
+++ ui/src/components/trips/planATrip.vue
@@ -400,7 +400,7 @@
              * @param editOutDate   the out date being edited.
              */
             saveDestination(row, editInDate, editOutDate) {
-                if (editInDate <= editOutDate || editInDate === null) {
+                if (editInDate === null || editOutDate === null || editInDate <= editOutDate) {
                     this.showDateError = false;
                     row.startDate = editInDate;
                     row.endDate = editOutDate;
