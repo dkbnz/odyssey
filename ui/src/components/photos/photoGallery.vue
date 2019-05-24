42855710ef8f26daaf1b6e40527265df41d0ecab
Joel Ridden <jnr26@uclive.ac.nz>
2019-05-24 14:56:03 +1200

    Added a popup on click for photos in the photo gallery, which is awaiting back end linking. Also allowed for the on click to pass the id of the photo clicked.
    #story[393] #pair[vjj14,jnr26]

--- ui/src/components/photos/photoGallery.vue
+++ ui/src/components/photos/photoGallery.vue
@@ -10,7 +10,7 @@
         <table style="margin-top:20px">
             <tr v-for="rowNumber in (amountOfRows)">
                 <td v-for="photo in getRowPhotos(rowNumber)">
-                    <b-img :src="photo.imgUrl" thumbnail></b-img>
+                    <b-img :src="photo.imgUrl" thumbnail @click="showImage(photo.id)"></b-img>
                 </td>
             </tr>
         </table>
@@ -19,6 +19,9 @@
                 :total-rows="rows"
                 :per-page="perPage"
         ></b-pagination>
+        <b-modal centered hide-footer ref="modalImage">
+            <b-img :src="getFullPhoto"></b-img>
+        </b-modal>
     </div>
 </template>
 
@@ -30,9 +33,10 @@
         data: function () {
             return {
                 photos: [],
-                rowSize: 2,
-                amountOfRows: 1,
+                rowSize: 6,
+                amountOfRows: 3,
                 currentPage: 0,
+                currentViewingID: 0
             }
         },
         computed: {
@@ -73,6 +77,13 @@
 
                 });
                 this.$refs['uploaderModal'].hide()
+            },
+
+            showImage() {
+                this.$refs['modalImage'].show();
+            },
+            getFullPhoto(id) {
+                return 'v1/photos/' + id;
             },
             /**
              * Creates the form data to send as the body of the POST request to the backend.
