d2ad60ead15b07de258b103f1973abc4a09547c7
Joel Ridden <jnr26@uclive.ac.nz>
2019-07-08 15:10:40 +1200

    Added the destination sidebar component. Included the tabs for your destinations and public destinations. Created a fetch method in your destinations to grab the logged in users destinations and fill a list group with their destination information
    #story[456] #pair[jnr26, dba62]

---
+++ ui/src/components/destinations/destinationSidebar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-tabs content-class="mt-3">
+            <b-tab title="Your Destinations" active><your-destinations :profile="profile"></your-destinations></b-tab>
+            <b-tab title="Public Destinations"><public-destinations :profile="profile"></public-destinations></b-tab>
+        </b-tabs>
+    </div>
+
+</template>
+
+<script>
+    import YourDestinations from "./yourDestinations";
+    import PublicDestinations from "./publicDestinations";
+    export default {
+        name: "destinationSidebar",
+        props: ['profile'],
+        components: {PublicDestinations, YourDestinations}
+    }
+</script>
+
+<style scoped>
+
+</style>
