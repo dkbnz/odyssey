e7af51a0e7a0865ae5eae8ff4d2bd347c22b2e6f
Cam <cta67@uclive.ac.nz>
2019-07-22 16:51:40 +1200

    Trying to fix git issues

---
+++ app/repositories/destinations/DestinationRepository.java
@@ -0,0 +1,99 @@
+package repositories.destinations;
+
+import io.ebean.Ebean;
+import models.Profile;
+import models.destinations.Destination;
+import models.photos.PersonalPhoto;
+import play.mvc.Result;
+
+import java.util.List;
+
+public class DestinationRepository {
+
+
+    private static final int DEFAULT_ADMIN_ID = 1;
+
+
+    private static final String PHOTO_FIELD = "photoGallery.photo";
+
+
+    /**
+     * Update the destination object.
+     *
+     * @param destination       the destination being updated.
+     */
+    public void update(Destination destination) {
+        destination.update();
+    }
+
+
+    /**
+     * Retrieve a Destination by its Id.
+     *
+     * @param destinationId     the Id of the destination being retrieved.
+     * @return                  the destination object corresponding with the destinationId.
+     */
+    public Destination fetch(Long destinationId) {
+        return Destination.find.byId(destinationId.intValue());
+    }
+
+
+    /**
+     * Finds all the destinations that contain a given photo.
+     *
+     * @param photo       the photo.
+     * @return            list of destinations containing the photo.
+     */
+    public List<Destination> fetch(PersonalPhoto photo) {
+        return Destination.find.query().where().eq(PHOTO_FIELD, photo).findList();
+    }
+
+
+    /**
+     * Deletes the destination specified.
+     *
+     * @param destination       The destination to delete from the database.
+     */
+    public void delete(Destination destination) {
+        // Clear the destination photos
+        destination.clearPhotoGallery();
+        destination.update();
+        // Delete destination
+        destination.delete();
+    }
+
+
+    /**
+     * Transfers the ownership of a destination to the default admin. Will be used when a public destination is used by
+     * another user.
+     *
+     * @param destination the destination to be changed ownership of.
+     */
+    public void transferDestinationOwnership(Destination destination) {
+        Profile defaultAdmin = Profile.find.byId(DEFAULT_ADMIN_ID);
+
+        destination.changeOwner(defaultAdmin);
+
+        update(destination);
+    }
+
+
+    /**
+     * Returns a list of Destinations that are equal, excluding the given Destination
+     *
+     * @param destination   Destination to search with.
+     * @return              List of destinations that are equal.
+     */
+    public List<Destination> findEqual(Destination destination) {
+        return Ebean.find(Destination.class)
+                .where()
+                .eq("name", destination.getName())
+                .eq("type", destination.getType())
+                .eq("district", destination.getDistrict())
+                .eq("latitude", destination.getLatitude())
+                .eq("longitude", destination.getLongitude())
+                .eq("country", destination.getCountry())
+                .ne("id", destination.getId())
+                .findList();
+    }
+}
---
+++ ui/src/components/destinations/destinationSearchList.vue
@@ -0,0 +1,147 @@
+<template>
+    <div>
+        <div v-if="showSearch">
+            <b-list-group>
+                <b-list-group-item class="flex-column align-items-start">
+                    <search-destinations v-if="showSearch"
+                                         :search-public="searchPublic"
+                                         :profile="profile"
+                                         :destinationTypes="destinationTypes"
+                                         @searched-destination="initialQuery"
+                    ></search-destinations>
+                </b-list-group-item>
+            </b-list-group>
+        </div>
+        <div v-else>
+            <b-button variant="success" @click="showSearch = true" block>Search</b-button>
+            <b-list-group class="scroll">
+                <b-list-group-item v-for="destination in (foundDestinations)" href="#" class="flex-column align-items-start" @click="$emit('destination-click', destination)">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">{{destination.name}}</h5>
+
+                        <div v-if="!searchPublic">
+                            <small v-if="destination.public">Public</small>
+                            <small v-else>Private</small>
+                        </div>
+                    </div>
+
+                    <p class="mb-1">
+                        {{destination.district}}
+                    </p>
+                    <p class="mb-1">
+                        {{destination.country}}
+                    </p>
+
+                </b-list-group-item>
+                <b-list-group-item href="#" class="flex-column justify-content-center">
+                    <div class="d-flex justify-content-center" v-if="loadingResults">
+                        <b-spinner label="Loading..."></b-spinner>
+                    </div>
+                    <div class="d-flex justify-content-center" v-else>
+                        <div v-if="moreResults">
+                            <b-button variant="success" @click="getMore" block>More</b-button>
+                        </div>
+                        <div v-else>
+                            <h5 class="mb-1">No More Results</h5>
+                        </div>
+                    </div>
+
+                </b-list-group-item>
+            </b-list-group>
+        </div>
+</div>
+</template>
+
+<script>
+import SearchDestinations from "./searchDestinationForm";
+export default {
+   name: "foundDestinations",
+   components: {SearchDestinations},
+   props: ['profile', 'destinationTypes', 'searchPublic'],
+    data() {
+       return {
+           foundDestinations: [],
+           showSearch: true,
+           loadingResults: true,
+           moreResults: true,
+           destToSearch: {},
+           queryPage: 0
+       }
+    },
+    methods: {
+
+       /**
+        * Function to retrieve more destinations when a user reaches the bottom of the list.
+        */
+       getMore() {
+           this.queryPage += 1;
+           this.queryDestinations(this.destToSearch);
+       },
+
+        /**
+         * Resets the data fields and performs the initial search query when a user clicks search
+         */
+       initialQuery(destinationToSearch) {
+           this.loadingResults = true;
+           this.showSearch = false;
+           this.queryPage = 0;
+           this.foundDestinations = [];
+           this.destToSearch = destinationToSearch;
+           this.queryDestinations(this.destToSearch);
+           this.loadingResults = false;
+       },
+        /**
+         * Runs a query which searches through the destinations in the database and returns all which
+         * follow the search criteria.
+         *
+         * @returns {Promise<Response | never>}
+         */
+        queryDestinations(destinationToSearch) {
+            let searchQuery =
+                "?name=" + destinationToSearch.name +
+                "&type_id=" + destinationToSearch.type +
+                "&district=" + destinationToSearch.district +
+                "&latitude=" + destinationToSearch.latitude +
+                "&longitude=" + destinationToSearch.longitude +
+                "&country=" + destinationToSearch.country +
+                (this.searchPublic ? "&is_public=1" : "&owner=" + this.profile.id) +
+                "&page=" + this.queryPage;
+
+            return fetch(`/v1/destinations` + searchQuery, {
+                dataType: 'html'
+            })
+                .then(this.checkStatus)
+                .then(this.parseJSON)
+                .then((data) => {
+                    if (data === undefined || data.length < 50) {
+                        this.moreResults = false;
+                    }
+                    for (var i = 0; i < data.length; i++) {
+                        this.foundDestinations.push(data[i]);
+                    }
+                })
+        },
+
+        checkStatus(response) {
+            if (response.status >= 200 && response.status < 300) {
+                return response;
+            }
+            const error = new Error(`HTTP Error ${response.statusText}`);
+            error.status = response.statusText;
+            error.response = response;
+            console.log(error);
+            throw error;
+        },
+
+        parseJSON(response) {
+            return response.json();
+        }
+    }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
