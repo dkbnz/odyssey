dfd64f6461131458501b47c39c3b45688509b5b1
Doug Barrett <mail+git@dkb.nz>
2019-07-24 18:17:15 +1200

    Added functionality for proposed travellertypes #story[456]

--- app/controllers/destinations/DestinationTravellerTypeController.java
+++ app/controllers/destinations/DestinationTravellerTypeController.java
@@ -1,12 +1,17 @@
 package controllers.destinations;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.inject.Inject;
 import models.TravellerType;
 import models.destinations.Destination;
+import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Result;
 import repositories.destinations.DestinationRepository;
 import repositories.destinations.TravellerTypeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static play.mvc.Results.ok;
 
@@ -21,15 +26,44 @@ public class DestinationTravellerTypeController {
         this.travellerTypeRepo = travellerTypeRepo;
     }
 
-    public Result create(Http.Request request, Long destinationId, Long travellerTypeId) {
+    /**
+     *
+     * @param request
+     * @param destinationId
+     * @return
+     */
+    public Result create(Http.Request request, Long destinationId) {
+
         Destination destinationToMutate = destinationRepo.fetch(destinationId);
-        TravellerType travellerTypeToAdd = travellerTypeRepo.findById(travellerTypeId);
-        destinationToMutate.addTravellerType(travellerTypeToAdd);
+
+        // Check for null
+        Set<TravellerType> travellerTypesToSet = new HashSet<>();
+
+        JsonNode body = request.body().asJson();
+
+        while (body.iterator().hasNext()) {
+            System.out.println(body.iterator().hasNext());
+            body.iterator().next();
+            travellerTypesToSet.add(Json.fromJson(body.iterator().next(), TravellerType.class));
+            body.iterator().remove();
+        }
+
+        destinationToMutate.setTravellerTypes(travellerTypesToSet);
+
         System.out.println(destinationToMutate.getTravellerTypes());
         destinationRepo.save(destinationToMutate);
+        destinationToMutate = destinationRepo.fetch(destinationId);
+        System.out.println(destinationToMutate.getTravellerTypes());
         return ok();
     }
 
+    /**
+     *
+     * @param request
+     * @param destinationId
+     * @param travellerTypeId
+     * @return
+     */
     public Result destroy(Http.Request request, Long destinationId, Long travellerTypeId) {
         Destination destinationToMutate = destinationRepo.fetch(destinationId);
         TravellerType travellerTypeToAdd = travellerTypeRepo.findById(travellerTypeId);
@@ -38,10 +72,32 @@ public class DestinationTravellerTypeController {
         return ok();
     }
 
-    public Result createRequest(Http.Request request, Long destinationId, Long travellerTypeId) {
+    /**
+     *
+     * @param request
+     * @param destinationId
+     * @param travellerTypeId
+     * @return
+     */
+    public Result createAddRequest(Http.Request request, Long destinationId, Long travellerTypeId) {
         Destination destinationToMutate = destinationRepo.fetch(destinationId);
         TravellerType travellerTypeToAdd = travellerTypeRepo.findById(travellerTypeId);
-        destinationToMutate.addProposedTravellerType(travellerTypeToAdd);
+        destinationToMutate.addProposedTravellerTypeAdd(travellerTypeToAdd);
+        destinationRepo.save(destinationToMutate);
+        return ok();
+    }
+
+    /**
+     *
+     * @param request
+     * @param destinationId
+     * @param travellerTypeId
+     * @return
+     */
+    public Result createRemoveRequest(Http.Request request, Long destinationId, Long travellerTypeId) {
+        Destination destinationToMutate = destinationRepo.fetch(destinationId);
+        TravellerType travellerTypeToAdd = travellerTypeRepo.findById(travellerTypeId);
+        destinationToMutate.addProposedTravellerTypeRemove(travellerTypeToAdd);
         destinationRepo.save(destinationToMutate);
         return ok();
     }
