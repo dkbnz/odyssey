9c941cac1b42d6c7ecf0a783184caed3a6f41c11
Joel Ridden <jnr26@uclive.ac.nz>
2019-07-24 19:30:39 +1200

    Added route and functionality to fetch all the destinations that have current proposals on them to be added to the admin panel #story[477]

--- app/controllers/destinations/DestinationTravellerTypeController.java
+++ app/controllers/destinations/DestinationTravellerTypeController.java
@@ -3,25 +3,33 @@ package controllers.destinations;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.inject.Inject;
+import models.Profile;
 import models.TravellerType;
 import models.destinations.Destination;
 import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Result;
+import repositories.ProfileRepository;
 import repositories.destinations.DestinationRepository;
 import repositories.destinations.TravellerTypeRepository;
 
+import static play.mvc.Results.forbidden;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import static play.mvc.Results.ok;
+import static play.mvc.Results.unauthorized;
 
 public class DestinationTravellerTypeController {
 
     private DestinationRepository destinationRepo;
     private TravellerTypeRepository travellerTypeRepo;
+    private ProfileRepository profileRepo;
+
+    private static final String AUTHORIZED = "authorized";
+    private static final String NOT_SIGNED_IN = "You are not logged in.";
 
     @Inject
     public DestinationTravellerTypeController(DestinationRepository destinationRepo, TravellerTypeRepository travellerTypeRepo) {
@@ -105,6 +113,24 @@ public class DestinationTravellerTypeController {
 
         destinationRepo.save(destinationToMutate);
         return ok();
+    }
+
+    /**
+     * Gets all the destinations that have traveller type proposals that are not currently accepted or rejected
+     *
+     * @param request the request from the front end that contains login info
+     * @return a json result of all the destinations with proposals
+     */
+    public Result fetchProposedDestinations(Http.Request request) {
+        return request.session()
+                .getOptional(AUTHORIZED)
+                .map(loggedInUserId -> {
+                    Profile loggedInUser = profileRepo.fetchSingleProfile(Integer.valueOf(loggedInUserId));
+                    if (!loggedInUser.getIsAdmin()) {
+                        return forbidden();
+                    }
+                    return ok(Json.toJson(destinationRepo.fetchProposed()));
+                }).orElseGet(() -> unauthorized(NOT_SIGNED_IN));
     }
 
 }
