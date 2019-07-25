af522333be40c77b7cb8a5ecbb8ba635983f90f3
hwm26 <hwm26@uclive.ac.nz>
2019-07-25 14:35:51 +1200

    Fixing code style issues brought up during code review #story[456]

--- app/models/trips/Trip.java
+++ app/models/trips/Trip.java
@@ -16,6 +16,10 @@ import java.util.List;
  */
 @Entity
 public class Trip extends BaseModel {
+    /**
+     * A finder used to search for a trip.
+     */
+    public static final Finder<Integer, Trip> find = new Finder<>(Trip.class);
 
     /**
      * The name of the trip.
@@ -23,9 +27,13 @@ public class Trip extends BaseModel {
     @Constraints.Required
     private String name;
 
+    /**
+     * The owner of the trip
+     */
     @ManyToOne()
     @JsonIgnore
     private Profile profile;
+
 
     public Profile getProfile() {
         return profile;
@@ -37,34 +45,48 @@ public class Trip extends BaseModel {
     @OneToMany(mappedBy="trip", cascade=CascadeType.ALL)
     private List<TripDestination> destinations;
 
+
     public String getName() {
         return name;
     }
+
 
     public void setName(String name) {
         this.name = name;
     }
 
+
     public List<TripDestination> getDestinations() {
         return destinations;
     }
+
 
     public void setDestinations(List<TripDestination> destinations) {
         this.destinations = destinations;
     }
 
+
+    /**
+     * Adds the given destination to the list of destinations.
+     *
+     * @param destination   destination to be added to list.
+     * @return              whether the add process was successful.
+     */
     public boolean addDestinations(TripDestination destination) {
         return destinations.add(destination);
     }
 
+
+    /**
+     * Removes the given destination from the list.
+     *
+     * @param destination   destination to be removed from the list.
+     * @return              whether the removal process was successful.
+     */
     public boolean removeDestinations(TripDestination destination) {
         return destinations.remove(destination);
     }
 
-    /**
-     * A finder used to search for a trip.
-     */
-    public static final Finder<Integer, Trip> find = new Finder<>(Trip.class);
 
     public static Finder<Integer, Trip> getFind() {
         return find;
