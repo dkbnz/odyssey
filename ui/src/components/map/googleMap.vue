d798949b246fcc5f6422dbc02feb1df5ab3303ab
Matthew <mjk141@uclive.ac.nz>
2019-07-25 18:17:03 +1200

    Slightly refined prop defaults for the map component.
    #story[469]

--- ui/src/components/map/googleMap.vue
+++ ui/src/components/map/googleMap.vue
@@ -9,42 +9,48 @@
     export default {
         name: "googleMap.vue",
         props: {
-            latitude: {
+            destinations: {
+                type: Array,
                 default() {
-                    return -41.272804;
+                    return []
                 }
             },
+            latitude: {
+                type: Number,
+                default: -41.272804
+            },
             longitude: {
+                type: Number,
                 default: 173.299565
             },
             zoom: {
+                type: Number,
                 default: 5
             },
             dragMarkers: {
                 default: false
-            },
-
+            }
         },
         components: {
-
         },
         mounted() {
             this.initMap();
-
         },
         data() {
-            return {
-            }
+            return {}
         },
         methods: {
             initMap() {
                 this.$map = new google.maps.Map(document.getElementById('map'), {
                     center: {lat: this.latitude, lng: this.longitude},
                     zoom: this.zoom
-                })
+                });
                 this.createMarker();
             },
             createMarker() {
+                for (let i = 0; i < this.destinations.length; i++) {
+                    console.log(i);
+                }
                 let marker = new google.maps.Marker({
                     position: {lat: -41.272804, lng: 173.299565},
                     map: this.$map,
