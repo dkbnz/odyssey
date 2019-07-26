b7d898e4601cb23c00ea6a569648180fadc1be60
Joel Ridden <jnr26@uclive.ac.nz>
2019-07-26 16:36:58 +1200

    Added colours to the markers for private and public destinations on the map
    #story[469] #pair[mjk141,jnr26]

--- ui/src/components/map/googleMap.vue
+++ ui/src/components/map/googleMap.vue
@@ -38,30 +38,57 @@
         },
         data() {
             return {
-                markerArray: []
+                markerArray: [],
+                publicMarker: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+                privateMarker: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
             }
         },
         watch: {
+            /**
+             * Watches the destinations list for changes and when there are changes calls the create marker function
+             */
             destinations: function() {
                 this.createMarker();
             }
         },
         methods: {
+            /**
+             * Initializes the map with given latitude and longitude and zoom
+             */
             initMap() {
                 this.$map = new google.maps.Map(document.getElementById('map'), {
                     center: {lat: this.latitude, lng: this.longitude},
                     zoom: this.zoom
                 });
             },
+
+            /**
+             * Creates all the markers for the map from the destinations array passed in props
+             */
             createMarker() {
-                for (let i = 0; i < this.destinations.length-1; i++) {
+                for (let i = 0; i < this.destinations.length; i++) {
+
+                    let currentDestination = this.destinations[i];
+                    let colour;
+                    if (currentDestination.public) {
+                        colour = this.publicMarker;
+                    } else {
+                        colour = this.privateMarker;
+                    }
                     let marker = new google.maps.Marker({
-                        position: {lat: this.destinations[i].latitude, lng: this.destinations[i].longitude},
+                        position: {lat: currentDestination.latitude, lng: currentDestination.longitude},
                         map: this.$map,
-                        title: this.destinations[i].name});
+                        title: currentDestination.name,
+                        icon: colour
+                    });
                     this.markerArray.push(marker);
                 }
             },
+
+
+            /**
+             * Clear's the markers currently displayed ont he map and clears the marker array
+             */
             clearMarkers() {
                 for (let i = 0; i < this.markerArray.length-1; i++) {
                     this.markerArray[i].setMap(null);
