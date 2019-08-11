07992a7f4389a7cba60d2ffdc119b101cc9725c7
Isaac Worsley <itw21@uclive.ac.nz>
2019-08-11 14:02:05 +1200

    Fixed some issues brought up by SonarLint analysis.

--- app/controllers/TravellerTypeController.java
+++ app/controllers/TravellerTypeController.java
@@ -1,16 +1,10 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
-import models.TravellerType;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import repositories.destinations.TravellerTypeRepository;
-
-import java.util.List;
 
 /**
  * Controller to handle CRUD of traveller types.
