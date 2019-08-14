33bbb42530c6844f0a1b6adb5c4d10996c4f4c0e
jnr26 <jnr26@uclive.ac.nz>
2019-08-14 16:38:59 +1200

    Fixed the merge issues from cherry picking and added map to the treasure hunts and removed some checks in destinations that were failing tests #story[524]

--- ui/src/components/treasureHunt/treasureHuntItem.vue
+++ ui/src/components/treasureHunt/treasureHuntItem.vue
@@ -153,7 +153,7 @@
                                     <google-map ref="map"
                                                 :showRadius="true"
                                                 :radius="radiusSelected.value"
-                                                :selectedRadiusDestination="displayedDestination">
+                                                :selectedRadiusDestination="selectedDestination">
                                     </google-map>
                                 </div>
                             </b-container>
