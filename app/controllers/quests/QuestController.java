d59f1a6ce75c3a36a67cf6185fc6f5f5fcd23082
Vinnie <vjj14@uclive.ac.nz>
2019-08-15 18:28:37 +1200

    Updated the validation and documentation for the retrieval of objectives, and implemented the fetching of all quests in the database and all quests owned by a specific user.
    #story[525]

--- app/controllers/quests/QuestController.java
+++ app/controllers/quests/QuestController.java
@@ -1,5 +1,6 @@
 package controllers.quests;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.google.protobuf.Api;
@@ -34,6 +35,18 @@ public class QuestController {
     }
 
 
+    /**
+     * Creates and saves a new objective for a user, checking if the user is creating one for themselves or if
+     * the user is an admin. It also checks the request for validity.
+     *
+     * @param request   the Http request containing a Json body of the new quest details.
+     * @param userId    the id of the user who will own the created quest.
+     * @return          created() (Http 201) response if creation is successful.
+     *                  notFound() (Http 404) response if a quest owner profile cannot be retrieved.
+     *                  forbidden() (Http 403) response if the user creating the quest is doing so incorrectly.
+     *                  badRequest() (Http 400) response if the request contains any errors in its form or contents.
+     *                  unauthorised() (Http 401) response if creation is being attempted while logged out of the app.
+     */
     public Result create(Http.Request request, Long userId) {
 
         Profile loggedInUser = AuthenticationUtil.validateAuthentication(profileRepository, request);
@@ -78,6 +91,67 @@ public class QuestController {
 
         return created(Json.toJson(newQuest));
     }
+
+
+    /**
+     * Retrieves all the quests stored in the database.
+     *
+     * @param request   the request from the front end of the application containing login information.
+     * @return          ok() (Http 200) response containing a Json body of the retrieved quests.
+     *                  badRequest() (Http 400) response containing an ApiError for an invalid Json body.
+     *                  unauthorized() (Http 401) response containing an ApiError if the user is not logged in.
+     */
+    public Result fetchAll(Http.Request request) {
+        Profile loggedInUser = AuthenticationUtil.validateAuthentication(profileRepository, request);
+        if (loggedInUser == null) {
+            return unauthorized(ApiError.unauthorized());
+        }
+
+        List<Quest> questQuery = questRepository.findAll();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String result;
+        try {
+            result = mapper
+                    .writerWithView(Views.Public.class)
+                    .writeValueAsString(questQuery);
+        } catch (JsonProcessingException e) {
+            return badRequest(ApiError.invalidJson());
+        }
+
+        return ok(result);
+    }
+
+
+    /**
+     * Retrieves all the quests owned by a specific user.
+     *
+     * @param request   the request from the front end of the application containing login information.
+     * @param ownerId   the id of the specific user whose quests are being retrieved.
+     * @return          ok() (Http 200) response containing the quests owned by the specified user.
+     *                  notFound() (Http 404) response containing an ApiError for retrieval failure.
+     *                  forbidden() (Http 401) response containing an ApiError for disallowed retrieval.
+     *                  unauthorized() (Http 401) response containing an ApiError if the user is not logged in.
+     */
+    public Result fetchByOwner(Http.Request request, Long ownerId) {
+        Profile loggedInUser = AuthenticationUtil.validateAuthentication(profileRepository, request);
+        if (loggedInUser == null) {
+            return unauthorized(ApiError.unauthorized());
+        }
+
+        Profile requestedUser = profileRepository.findById(ownerId);
+
+        if (requestedUser == null) {
+            return notFound(ApiError.notFound());
+        }
+
+        if (!AuthenticationUtil.validUser(loggedInUser, requestedUser)) {
+            return forbidden(ApiError.forbidden());
+        }
+
+        return ok(Json.toJson(requestedUser.getMyQuests()));
+    }
+
 
 
 
