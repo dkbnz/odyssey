263dbece34a702a6682a29aeb57bbc5c2ca90339
Vinnie <vjj14@uclive.ac.nz>
2019-08-16 16:08:38 +1200

    Added test steps for the quest create method and made some subsequent changes within the method.
    #story[525] #pair[vjj14,dba62]

--- test/steps/GeneralSteps.java
+++ test/steps/GeneralSteps.java
@@ -238,4 +238,19 @@ public class GeneralSteps {
         Assert.assertEquals(expectedStatusCode, testContext.getStatusCode());
     }
 
+    @Then("the following ApiErrors are returned")
+    public void theFollowingApiErrorsAreReturned(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+
+        List<String> errors = dataTable.asList();
+
+        throw new cucumber.api.PendingException();
+    }
+
 }
