bcf191cb09221b78f175cdd686c2239e650b419e
jnr26 <jnr26@uclive.ac.nz>
2019-08-16 17:41:30 +1200

    Added repository model for finding a list of quests that use said country. #pair[dba62, jnr26] #story[525]

--- app/repositories/quests/QuestRepository.java
+++ app/repositories/quests/QuestRepository.java
@@ -5,6 +5,8 @@ import io.ebean.BeanRepository;
 import io.ebean.Ebean;
 import models.quests.Quest;
 
+import java.util.List;
+
 
 public class QuestRepository extends BeanRepository<Long, Quest> {
 
@@ -13,4 +15,13 @@ public class QuestRepository extends BeanRepository<Long, Quest> {
         super(Quest.class, Ebean.getDefaultServer());
     }
 
+    /**
+     * Retrieve a List of Quests that contain an Objective with the given country string as the Destination.
+     *
+     * @param country   the Country to find.
+     * @return          a List of Quests that have the given country as an occurrence.
+     */
+    public List<Quest> findUsing(String country) {
+        return query().where().in("countryOccurrences.key", country).findList();
+    }
 }
