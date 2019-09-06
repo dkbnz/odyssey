aabbef106d053fa28511f48154334b0cd6b315d2
Isaac Worsley <itw21@uclive.ac.nz>
2019-09-07 08:16:51 +1200

    Fixed styling for homepage with subtitle section. Now all works as expected. Some cool animations are now here. #story[612]

--- ui/src/components/index/indexPage.vue
+++ ui/src/components/index/indexPage.vue
@@ -6,31 +6,31 @@
                     <img class="logoTranslate" alt="TravelEA Logo" src="../../../static/logo.png">
                     <h1 class="logoTextAppear">TravelEA</h1>
                 </div>
-            </div>
-            <div class="subtitleSection">
-                <h3 align="center" id="subtitle">Your personal Travel Executive Assistant!</h3>
-                <b-row>
-                    <b-col>
-                        <b-button class="btn btn-info btn-lg float-right" v-b-modal.modalSignup>Sign Up</b-button>
-                        <b-modal centered hide-footer id="modalSignup">
-                            <template slot="modal-title"><h2>Sign Up</h2></template>
-                            <signup :createdByAdmin="false"
-                                    v-bind:nationalityOptions="nationalityOptions"
-                                    v-bind:travTypeOptions="travTypeOptions">
-                            </signup>
-                        </b-modal>
-                    </b-col>
 
-                    <b-col>
-                        <b-button class="btn btn-info btn-lg float-left" v-b-modal.modalLogin>Login</b-button>
-                        <b-modal centered hide-footer id="modalLogin" title="Login">
-                            <template slot="modal-title"><h2>Login</h2></template>
-                            <login></login>
-                        </b-modal>
-                    </b-col>
-                </b-row>
-            </div>
+                <div class="subtitleSection">
+                    <h3 id="subtitle">Your personal Travel Executive Assistant!</h3>
+                    <b-row>
+                        <b-col>
+                            <b-button class="btn btn-info btn-lg float-right" v-b-modal.modalSignup>Sign Up</b-button>
+                            <b-modal centered hide-footer id="modalSignup">
+                                <template slot="modal-title"><h2>Sign Up</h2></template>
+                                <signup :createdByAdmin="false"
+                                        v-bind:nationalityOptions="nationalityOptions"
+                                        v-bind:travTypeOptions="travTypeOptions">
+                                </signup>
+                            </b-modal>
+                        </b-col>
 
+                        <b-col>
+                            <b-button class="btn btn-info btn-lg float-left" v-b-modal.modalLogin>Login</b-button>
+                            <b-modal centered hide-footer id="modalLogin" title="Login">
+                                <template slot="modal-title"><h2>Login</h2></template>
+                                <login></login>
+                            </b-modal>
+                        </b-col>
+                    </b-row>
+                </div>
+            </div>
         </div>
     </div>
 </template>
