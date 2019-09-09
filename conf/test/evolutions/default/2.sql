5523027087440175a59af527e653f39a5e52096a
Isaac Worsley <itw21@uclive.ac.nz>
2019-09-09 14:22:54 +1200

    Kept breaking down tests for badges. Have commented out for now, waiting for the backend.
    #story[613]

--- conf/test/evolutions/default/2.sql
+++ conf/test/evolutions/default/2.sql
@@ -577,7 +577,7 @@ INSERT INTO `quest_attempt` (`id`, `attempted_by_id`, `quest_attempted_id`, `sol
 (4, 2, 4, 0, 1, 0),
 (5, 3, 5, 0, 2, 1),
 (6, 4, 2, 0, 2, 1),
-(7, 2, 6, 0, 1, 0);
+(7, 7, 6, 1, 0, 0);
 
 
 # --- !Downs
