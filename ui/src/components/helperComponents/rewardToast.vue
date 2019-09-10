6f52c3f1270de218b494aa8a0bd3a91a12fa4b06
Doug Barrett <mail+git@dkb.nz>
2019-09-10 18:44:48 +1200

    Integrated badge success display toast with request response. No displays the same b-list-group as on the dash page.
    #story[613] #pair[dba62, itw21]

--- ui/src/components/helperComponents/rewardToast.vue
+++ ui/src/components/helperComponents/rewardToast.vue
@@ -1,19 +1,57 @@
 <template>
     <div>
         <!--<p>{{rewardJson.pointsRewarded}}</p>-->
-        {{rewardJson}}
-        <!--<div v-for="badge in rewardJson.badgesAchieved">-->
-            <!--<p>{{badge}}</p>-->
+        <b-list-group>
+            <b-list-group-item
+                               class="flex-column align-items-start"
+                               v-for="badge in rewardJson.badgesAchieved"
+                               :key="badge.id">
+                <b-row>
+                    <b-col cols="4">
+                        <single-badge :badge="badge"></single-badge>
+                    </b-col>
+                    <b-col>
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{{badge.name}}</h5>
+                            <small>{{badge.progress}}{{badge.breakpoint == null ? '' : '/' + badge.breakpoint}}</small>
+                        </div>
+
+                        <p class="mb-1">
+                            {{badge.howToProgress}}
+                        </p>
+
+                        <b-progress :value="badge.progress" :max="badge.breakpoint == null ? badge.progress : badge.breakpoint" :variant="badge.breakpoint == null ? 'success' : 'primary'"></b-progress>
+                    </b-col>
+                </b-row>
+            </b-list-group-item>
+        </b-list-group>
+
+        <!--<div v-for="badge in rewardJson.badgesAchieved" class="w-25">-->
+            <!--<h3>{{badge.name}}</h3>-->
+            <!--<small>{{rewardLevels[badge.level-1]}}</small>-->
+            <!--<single-badge :badge="badge"></single-badge>-->
         <!--</div>-->
     </div>
 </template>
 
 <script>
+    import SingleBadge from "../badges/singleBadge";
     export default {
+        name: "rewardToast",
+
         props: {
             rewardJson: Object
         },
-        name: "rewardToast"
+
+        data() {
+            return {
+                rewardLevels: ['Bronze', 'Silver', 'Gold']
+            }
+        },
+
+        components: {
+            SingleBadge
+        },
     }
 </script>
 
