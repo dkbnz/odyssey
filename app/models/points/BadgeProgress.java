a1ee128205718ad1fb7c05027f3daf5e121b05e5
Isaac Worsley <itw21@uclive.ac.nz>
2019-09-11 01:08:41 +1200

    Fixing SonarLint issues. #story[613]

--- app/models/points/BadgeProgress.java
+++ app/models/points/BadgeProgress.java
@@ -1,13 +1,11 @@
 package models.points;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import models.destinations.Destination;
 import models.util.BaseModel;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
 public class BadgeProgress extends BaseModel {
