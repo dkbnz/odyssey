e52c07686d46582c43fe085ddbb051a7f46a7580
Vinnie <vjj14@uclive.ac.nz>
2019-09-17 13:02:21 +1200

    Updated hint and vote models to be entities and updated the db to include these.
    #story[614] #pair[vjj14, mpo74]

--- app/models/hints/Hint.java
+++ app/models/hints/Hint.java
@@ -8,14 +8,15 @@ import models.util.BaseModel;
 import util.Views;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import java.util.Collection;
 
 /**
  * Class for hints that are used to help solve an objective.
  */
+@Entity
 public class Hint extends BaseModel {
 
     /**
