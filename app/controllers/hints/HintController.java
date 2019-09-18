f3170be3cb21d40415570a98afb7920e8076446b
Matilda Porterfield <mpo74@uclive.ac.nz>
2019-09-18 23:15:29 +1200

    Added the implementation for the create method in the hint controller. Updated tests so almost all hint tests pass. Still need to implement when a user has solved an objective for a non completed quest. Updated the test data for hint and quest tests.
    #story[614]

--- app/controllers/hints/HintController.java
+++ app/controllers/hints/HintController.java
@@ -1,13 +1,49 @@
 package controllers.hints;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.hints.Hint;
+import models.objectives.Objective;
+import models.profiles.Profile;
+import models.util.ApiError;
+import models.util.Errors;
+import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Result;
+import repositories.objectives.HintRepository;
+import repositories.objectives.ObjectiveRepository;
+import repositories.profiles.ProfileRepository;
+import util.AuthenticationUtil;
+import util.Views;
 
-import static play.mvc.Results.created;
-import static play.mvc.Results.ok;
+import javax.inject.Inject;
+
+import java.util.List;
+
+import static play.mvc.Results.*;
 
 public class HintController {
 
+    private ProfileRepository profileRepository;
+    private HintRepository hintRepository;
+    private ObjectiveRepository objectiveRepository;
+    private ObjectMapper objectMapper;
+
+    /**
+     * String constants for json creation.
+     */
+    private static final String NEW_HINT_ID = "newHintId";
+
+    @Inject
+    public HintController(ProfileRepository profileRepository,
+                          HintRepository hintRepository,
+                          ObjectiveRepository objectiveRepository,
+                          ObjectMapper objectMapper) {
+        this.profileRepository = profileRepository;
+        this.hintRepository = hintRepository;
+        this.objectiveRepository = objectiveRepository;
+        this.objectMapper = objectMapper;
+    }
 
     /**
      * Creates a new hint for an objective in a quest.
@@ -19,7 +55,51 @@ public class HintController {
      * @return
      */
     public Result create(Http.Request request, Long objectiveId, Long userId) {
-        return created();
+        Profile loggedInUser = AuthenticationUtil.validateAuthentication(profileRepository, request);
+        if (loggedInUser == null) {
+            return unauthorized(ApiError.unauthorized());
+        }
+
+        Objective objectiveToAddHint = objectiveRepository.findById(objectiveId);
+        if (objectiveToAddHint == null) {
+            return notFound(ApiError.notFound(Errors.OBJECTIVE_NOT_FOUND));
+        }
+
+        Profile hintCreator = profileRepository.findById(userId);
+        if (hintCreator == null) {
+            return notFound(ApiError.notFound(Errors.PROFILE_NOT_FOUND));
+        }
+
+        // Can create a hint if an admin, the owner, or have solved the objective
+        List<Objective> solvedObjectives = objectiveRepository.findAllCompletedUsing(hintCreator);
+        boolean objectiveSolved = solvedObjectives.contains(objectiveToAddHint);
+
+        if (!(AuthenticationUtil.validUser(loggedInUser, hintCreator) || objectiveSolved)) {
+            return forbidden(ApiError.forbidden());
+        }
+
+        Hint hint;
+
+        try {
+            // Attempt to turn json body into a hint object.
+            hint = objectMapper.readerWithView(Views.Owner.class)
+                    .forType(Hint.class)
+                    .readValue(request.body().asJson());
+        } catch (Exception e) {
+            // Errors with deserialization.
+            return badRequest(ApiError.badRequest(Errors.INVALID_JSON_FORMAT));
+        }
+
+        ObjectNode returnJson = objectMapper.createObjectNode();
+
+        hintRepository.save(hint);
+
+        returnJson.set(NEW_HINT_ID, Json.toJson(hint.getId()));
+
+        profileRepository.update(hintCreator);
+        objectiveRepository.update(objectiveToAddHint);
+
+        return created(returnJson);
     }
 
 
