2d3433edacd7ef37a1d8e116d94a03ec8be2b4cb
Vinnie <vjj14@uclive.ac.nz>
2019-09-18 20:07:17 +1200

    Added the route for hint creation, the data table parser and the createRequest for the tests, and made other corrections to documentation and code.
    #story[614]

--- test/steps/HintTestSteps.java
+++ test/steps/HintTestSteps.java
@@ -1,15 +1,28 @@
 package steps;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import models.objectives.Objective;
 import models.profiles.Profile;
 import org.junit.Assert;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.Helpers;
 import repositories.objectives.ObjectiveRepository;
 import repositories.profiles.ProfileRepository;
 
+import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.fail;
+import static play.test.Helpers.POST;
+import static play.test.Helpers.fakeRequest;
+import static play.test.Helpers.route;
 
 public class HintTestSteps {
 
@@ -20,9 +33,31 @@ public class HintTestSteps {
 
 
     /**
+     * Authorisation token for sessions
+     */
+    private static final String AUTHORIZED = "authorized";
+
+
+    /**
+     * The objective uri.
+     */
+    private static final String OBJECTIVE_URI = "/v1/objectives/";
+
+
+    /**
+     * The hints uri.
+     */
+    private static final String HINTS_URI = "/hints/";
+
+
+    /**
      * New instance of the general test steps.
      */
     private GeneralTestSteps generalTestSteps = new GeneralTestSteps();
+
+
+    private static final String MESSAGE_STRING = "Message";
+    private static final String HINT = "hint";
 
 
     /**
@@ -38,17 +73,56 @@ public class HintTestSteps {
             testContext.getApplication().injector().instanceOf(ProfileRepository.class);
 
 
+    /**
+     * Converts a given data table containing a hint into a json node object.
+     *
+     * @param dataTable     the data table containing the hint message.
+     * @param index         the position in the data table the json components are extracted from.
+     * @return              a JsonNode of a hint containing information extracted from the data table.
+     */
+    private JsonNode convertDataTableToHintJson(io.cucumber.datatable.DataTable dataTable, int index) {
+        List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
+        String hintMessage = list.get(index).get(MESSAGE_STRING);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode json = mapper.createObjectNode();
+        json.put(HINT, hintMessage);
+
+        return json;
+    }
+
+
+    /**
+     * Sends a request to create a hint from the given json node.
+     *
+     * @param json              a JsonNode containing the body of the request.
+     * @param objectiveId       the Id of the objective the hint is created for.
+     */
+    private void createHintRequest(JsonNode json, int objectiveId) {
+        Http.RequestBuilder request = fakeRequest()
+                .method(POST)
+                .bodyJson(json)
+                .uri(OBJECTIVE_URI + objectiveId + HINTS_URI + testContext.getTargetId())
+                .session(AUTHORIZED, testContext.getLoggedInId());
+        Result result = route(testContext.getApplication(), request);
+        testContext.setStatusCode(result.status());
+    }
+
+
     @Given("^I own the objective with id (\\d+)$")
     public void iOwnTheObjectiveWithId(Integer objectiveId) {
         Objective objective = objectiveRepository.findById(Long.valueOf(objectiveId));
         Assert.assertEquals(testContext.getLoggedInId(), objective.getOwner().getId().toString());
     }
 
+
     @Given("^I do not own the objective with id (\\d+)$")
     public void iDoNotOwnTheObjectiveWithId(Integer objectiveId) {
         Objective objective = objectiveRepository.findById(Long.valueOf(objectiveId));
         Assert.assertNotEquals(testContext.getLoggedInId(), objective.getOwner().getId().toString());
     }
+
 
     @Given("^I have solved the objective with id (\\d+)$")
     public void iHaveSolvedTheObjectiveWithId(Integer objectiveId) {
@@ -57,6 +131,7 @@ public class HintTestSteps {
         Objective objective = objectiveRepository.findById(Long.valueOf(objectiveId));
         Assert.assertTrue(solvedObjectives.contains(objective));
     }
+
 
     @Given("^I have not solved the objective with id (\\d+)$")
     public void iHaveNotSolvedTheObjectiveWithId(Integer objectiveId) {
@@ -69,6 +144,10 @@ public class HintTestSteps {
 
     @When("^I attempt to create a hint with the following values for the objective with id (\\d+)$")
     public void iAttemptToCreateAHintWithTheFollowingValuesForTheObjectiveWithId(Integer objectiveId, io.cucumber.datatable.DataTable dataTable) {
-//        throw new cucumber.api.PendingException()
+        testContext.setTargetId(testContext.getLoggedInId());
+        for (int i = 0; i < dataTable.height() - 1; i++) {
+            JsonNode json = convertDataTableToHintJson(dataTable, i);
+            createHintRequest(json, objectiveId);
+        }
     }
 }
