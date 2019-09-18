5bfb6a9ea4595195b78e87e2640b22d0aafeb1d1
Isaac Worsley <itw21@uclive.ac.nz>
2019-09-18 14:07:28 +1200

    Changed country regex and radius of earth to global variables #story[613]

--- app/controllers/points/AchievementTrackerController.java
+++ app/controllers/points/AchievementTrackerController.java
@@ -20,7 +20,6 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
-import repositories.points.AchievementTrackerRepository;
 import repositories.points.BadgeRepository;
 import repositories.points.PointRewardRepository;
 import repositories.profiles.ProfileRepository;
@@ -42,8 +41,8 @@ public class AchievementTrackerController extends Controller {
 
     private static final int SINGLE_COUNTRY = 1;
     private static final int INCREMENT_ONE = 1;
-
     private static final int ADVENTURER_THRESHOLD = 10;
+    private static final int RADIUS_OF_THE_EARTH = 6371;
 
 
     private ProfileRepository profileRepository;
@@ -408,16 +407,13 @@ public class AchievementTrackerController extends Controller {
      * @return              a double containing the distance between the two points.
      */
     private double calculateDistance(double latitude1, double latitude2, double longitude1, double longitude2) {
-
-        final int R = 6371; // Radius of the earth
-
         double latDistance = Math.toRadians(latitude2 - latitude1);
         double lonDistance = Math.toRadians(longitude2 - longitude1);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
                 + Math.cos(Math.toRadians(latitude1)) * Math.cos(Math.toRadians(latitude2))
                 * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = R * c * 1000; // convert to meters
+        double distance = RADIUS_OF_THE_EARTH * c * 1000; // convert to meters
         distance = Math.pow(distance, 2);
 
         return Math.sqrt(distance);
