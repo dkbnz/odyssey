934f346b7c352a331debb9dbecd059813907241d
Vinnie <vjj14@uclive.ac.nz>
2019-09-19 21:02:24 +1200

    Finished off the test scenarios, but need to perform the corresponding database insertions to ensure correctness, implemented the retrieval method and its validation, and modified the Hint model.
    #story[614]

--- test/features/hint.feature
+++ test/features/hint.feature
@@ -98,30 +98,22 @@ Feature: Hint API Endpoint
       | Requested objective not found. |
 
 
-
-
-
-
-
-
-
-
-
+#  TODO: Vinnie, determine why these cases are failing (most probably database-related)
 #  Scenario: Successfully retrieving all hints for an objective I have completed
 #    Given the application is running
 #    And I am logged in
 #    And an objective exists with id 29
-#    And I have completed the objective with id 29
+#    And I have solved the objective with id 29
 #    When I attempt to retrieve all hints for the objective with id 29
 #    Then the status code received is 200
-#    And the response contains n hints
+#    And the response contains 2 hints
 #
 #
 #  Scenario: Successfully retrieving all hints for an objective I have completed with no hints
 #    Given the application is running
 #    And I am logged in
 #    And an objective exists with id 29
-#    And I have completed the objective with id 29
+#    And I have solved the objective with id 29
 #    When I attempt to retrieve all hints for the objective with id 29
 #    Then the status code received is 200
 #    And the response contains 0 hints
@@ -131,14 +123,57 @@ Feature: Hint API Endpoint
 #    Given the application is running
 #    And I am logged in as an admin user
 #    And an objective exists with id 29
-#    And I have not completed the objective with id 29
+#    And I have not solved the objective with id 29
 #    When I attempt to retrieve all hints for the objective with id 29
 #    Then the status code received is 200
-#    And the response contains n hints
-#
-#
-#  Scenario: Unsuccessfully retrieving all hints for an objective I have not completed
-#
-#  Scenario: Unsuccessfully retrieving all hints for an objective that does not exist
-#
-#  Scenario: Unsuccessfully retrieving all hints for an objective when not logged in
+#    And the response contains 2 hints
+
+
+  Scenario: Unsuccessfully retrieving all hints for an objective I have not completed
+    Given the application is running
+    And I am logged in
+    And an objective exists with id 29
+    And I have not solved the objective with id 29
+    When I attempt to retrieve all hints for the objective with id 29
+    Then the status code received is 403
+    And the following ApiErrors are returned
+      | You are not authorized to access this resource. |
+
+
+  Scenario: Unsuccessfully retrieving all hints for an objective that does not exist
+    Given the application is running
+    And I am logged in
+    And an objective does not exist with id 40
+    When I attempt to retrieve all hints for the objective with id 40
+    Then the status code received is 404
+    And the following ApiErrors are returned
+      | Requested objective not found. |
+
+
+  Scenario: Unsuccessfully retrieving all hints for an objective when not logged in
+    Given the application is running
+    And I am not logged in
+    And an objective exists with id 29
+    When I attempt to retrieve all hints for the objective with id 29
+    Then the status code received is 401
+    And the following ApiErrors are returned
+      | You are not logged in. |
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/steps/HintTestSteps.java
+++ test/steps/HintTestSteps.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import models.objectives.Objective;
 import models.profiles.Profile;
@@ -20,9 +21,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import static play.test.Helpers.POST;
-import static play.test.Helpers.fakeRequest;
-import static play.test.Helpers.route;
+import static play.test.Helpers.*;
 
 public class HintTestSteps {
 
@@ -47,8 +46,7 @@ public class HintTestSteps {
     /**
      * The hints uri.
      */
-    private static final String HINTS_URI = "/hints/";
-
+    private static final String HINTS_URI = "/hints";
 
 
     private static final String MESSAGE_STRING = "Message";
@@ -89,9 +87,9 @@ public class HintTestSteps {
     /**
      * Converts a given data table containing a hint into a json node object.
      *
-     * @param dataTable     the data table containing the hint message.
-     * @param index         the position in the data table the json components are extracted from.
-     * @return              a JsonNode of a hint containing information extracted from the data table.
+     * @param dataTable the data table containing the hint message.
+     * @param index     the position in the data table the json components are extracted from.
+     * @return a JsonNode of a hint containing information extracted from the data table.
      */
     private JsonNode convertDataTableToHintJson(io.cucumber.datatable.DataTable dataTable, int index) {
         List<Map<String, String>> list = dataTable.asMaps(String.class, String.class);
@@ -108,15 +106,31 @@ public class HintTestSteps {
     /**
      * Sends a request to create a hint from the given json node.
      *
-     * @param json              a JsonNode containing the body of the request.
-     * @param objectiveId       the Id of the objective the hint is created for.
+     * @param json        a JsonNode containing the body of the request.
+     * @param objectiveId the Id of the objective the hint is created for.
      */
     private void createHintRequest(JsonNode json, int objectiveId) {
         Http.RequestBuilder request = fakeRequest()
                 .method(POST)
                 .bodyJson(json)
-                .uri(OBJECTIVE_URI + objectiveId + HINTS_URI + testContext.getTargetId())
+                .uri(OBJECTIVE_URI + objectiveId + HINTS_URI + "/" + testContext.getTargetId())
                 .session(AUTHORIZED, testContext.getLoggedInId());
+        Result result = route(testContext.getApplication(), request);
+        testContext.setStatusCode(result.status());
+        testContext.setResponseBody(Helpers.contentAsString(result));
+    }
+
+
+    /**
+     * Sends a request to retrieve all hints for an objective with the Id specified.
+     *
+     * @param objectiveId the Id of of the objective that is having its hints retrieved.
+     */
+    private void fetchAllHintsRequest(int objectiveId) {
+        Http.RequestBuilder request = fakeRequest()
+                .method(GET)
+                .session(AUTHORIZED, testContext.getLoggedInId())
+                .uri(OBJECTIVE_URI + objectiveId + HINTS_URI);
         Result result = route(testContext.getApplication(), request);
         testContext.setStatusCode(result.status());
         testContext.setResponseBody(Helpers.contentAsString(result));
@@ -183,5 +197,16 @@ public class HintTestSteps {
             JsonNode json = convertDataTableToHintJson(dataTable, i);
             createHintRequest(json, objectiveId);
         }
+    }
+
+    @When("^I attempt to retrieve all hints for the objective with id (\\d+)$")
+    public void iAttemptToRetrieveAllHintsForTheObjectiveWithId(Integer objectiveId) {
+        fetchAllHintsRequest(objectiveId);
+    }
+
+
+    @Then("^The response contains (\\d+) hints$")
+    public void theResponseContainsHints(Integer expectedNumberOfHints) {
+        assert(expectedNumberOfHints == testContext.getResponseBody().length());
     }
 }
