bdfc4cfc141551fc3f1ee459e825b996f692914d
Doug <dba62@uclive.ac.nz>
2019-09-22 15:31:59 +1200

    Implemented method for the upvote and downvote of a hint.
    #story[614]

--- app/repositories/hints/VoteRepository.java
+++ app/repositories/hints/VoteRepository.java
@@ -13,12 +13,29 @@ import models.profiles.Profile;
  */
 public class VoteRepository extends BeanRepository<Long, Vote> {
 
+    private static final String VOTER = "voter";
+    private static final String TARGET_HINT = "targetHint";
+
     @Inject
     public VoteRepository() {
         super(Vote.class, Ebean.getDefaultServer());
     }
 
+
+    /**
+     * Retrieves a vote that contains the given user and hint.
+     * Used to check if a user has already voted on a hint or to change a users vote.
+     *
+     * @param voter         the profile to find.
+     * @param targetHint    the hint to find.
+     * @return              the corresponding vote.
+     *                      null if vote not found.
+     */
     public Vote findUsing(Profile voter, Hint targetHint) {
-        return query().where().eq("voter", voter).eq("targetHint", targetHint).findOne();
+        return query()
+                .where()
+                .eq(VOTER, voter)
+                .eq(TARGET_HINT, targetHint)
+                .findOne();
     }
 }
