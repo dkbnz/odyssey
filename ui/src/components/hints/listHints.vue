4a074fd1467bb05ca55104b94372eaa88a5c7070
jnr26 <jnr26@uclive.ac.nz>
2019-09-24 14:36:59 +1200

    Added pagination for hints on completed quests for the front end. This is beneficial so that load speeds become faster for users of our application and less traffic on their network is being used. #story[614]

--- ui/src/components/hints/listHints.vue
+++ ui/src/components/hints/listHints.vue
@@ -24,6 +24,28 @@
                 </b-row>
             </b-list-group-item>
             <b-row no-gutters class="mt-2">
+                <b-col cols="3">
+                    <b-form-group
+                            id="numItemsPast-field"
+                            label-for="perPagePast">
+                        <b-form-select :options="optionViews" id="perPage" size="sm" trim v-model="perPage">
+                        </b-form-select>
+                    </b-form-group>
+                </b-col>
+                <b-col cols="8">
+                    <b-pagination
+                            :per-page="perPage"
+                            :total-rows="rows"
+                            align="center"
+                            aria-controls="my-table"
+                            first-text="First"
+                            last-text="Last"
+                            size="sm"
+                            v-model="currentPage"
+                    ></b-pagination>
+                </b-col>
+            </b-row>
+            <b-row no-gutters class="mt-2">
                 <b-col>
                     <div class="float-right">
                         <b-button class="no-wrap-text" size="sm" variant="primary" @click="addHint" v-if="solved">Add Hint</b-button>
@@ -49,6 +71,42 @@
             profile: Object,
             hints: Array,
             solved: false
+        },
+
+        data: function () {
+            return {
+                optionViews: [
+                    {value: 1, text: "1"},
+                    {value: 5, text: "5"},
+                    {value: 10, text: "10"},
+                    {value: 15, text: "15"}
+                ],
+                perPage: 5,
+                currentPage: 1
+            }
+        },
+
+        computed: {
+          rows() {
+              if (this.solved) {
+                  return this.objective.numberOfHints;
+              }
+              return this.hints.length;
+          }
+        },
+
+        watch: {
+            currentPage() {
+                if (this.solved) {
+                    this.$emit('request-new-hints-page', this.currentPage, this.perPage)
+                }
+            },
+
+            perPage() {
+                if (this.solved) {
+                    this.$emit('request-new-hints-page', this.currentPage, this.perPage)
+                }
+            }
         },
 
         methods: {
